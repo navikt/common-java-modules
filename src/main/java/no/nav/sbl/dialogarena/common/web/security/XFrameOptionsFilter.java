9922fb78a380e85bdb629547acb73c524a9868b0
Stefan Landrø <Stefan.Landro@nav.no>
2013-04-04 11:39:21 +0000

    La til felles sikkerhetsfilter
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/common/trunk@17231 6ec884bf-e345-0410-bd42-d282eb737ba0

---
+++ src/main/java/no/nav/sbl/dialogarena/common/web/security/XFrameOptionsFilter.java
@@ -0,0 +1,65 @@
+package no.nav.sbl.dialogarena.common.web.security;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Legger til X-Frame-Options på responsen for å unngå click-jacking angrep
+ * <p/>
+ * Setter DENY per default - noe som betyr at filtrerte ressurser ikke kan frames
+ * <p/>
+ * http://tools.ietf.org/html/draft-ietf-websec-x-frame-options-01
+ */
+public class XFrameOptionsFilter implements Filter {
+
+    public static final String OPTION_INIT_PARAMETER_NAME = "option";
+
+    static final String DENY_OPTION = "DENY";
+    static final String SAMEORIGIN_OPTION = "SAMEORIGIN";
+
+    Set<String> availableOptions = new HashSet<String>();
+
+    {
+        availableOptions.add(DENY_OPTION);
+        availableOptions.add(SAMEORIGIN_OPTION);
+    }
+
+    static final String DEFAULT_OPTION = DENY_OPTION;
+
+    static final String X_FRAME_OPTIONS_HEADER_NAME = "X-Frame-Options";
+
+    private String option = DEFAULT_OPTION;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        String option = filterConfig.getInitParameter(OPTION_INIT_PARAMETER_NAME);
+        if (option != null && !option.isEmpty()) {
+
+            if (availableOptions.contains(option)) {
+                this.option = option;
+            } else {
+                throw new IllegalArgumentException("Init parameter option with value " + option + " is illegal.");
+            }
+        }
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletResponse.setHeader(X_FRAME_OPTIONS_HEADER_NAME, this.option);
+        chain.doFilter(request, httpServletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
