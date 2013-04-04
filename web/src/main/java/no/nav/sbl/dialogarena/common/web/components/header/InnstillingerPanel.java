22021885400020236231c4934b13091811acf244
Stian Landsnes <Stian.Landsnes@nav.no>
2013-04-04 11:14:20 +0000

    MOD-3601: ny headerkomponent, InnstillingerPanel
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/common/trunk@17228 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/main/java/no/nav/sbl/dialogarena/common/web/components/header/InnstillingerPanel.java
+++ web/src/main/java/no/nav/sbl/dialogarena/common/web/components/header/InnstillingerPanel.java
@@ -1,11 +1,13 @@
 package no.nav.sbl.dialogarena.common.web.components.header;
 
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 
 public class InnstillingerPanel extends Panel {
 
     public InnstillingerPanel(String id) {
         super(id);
+        add(new Label("label", "innstillinger"));
     }
 
 }
