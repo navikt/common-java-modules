cf86b951300c2148542f1727554b6c0f1f2bce95
Stian Landsnes <Stian.Landsnes@nav.no>
2013-04-05 12:05:16 +0000

    MOD-3600: ny headerkomponent, InnstillingerPanel
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/common/trunk@17296 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/main/java/no/nav/sbl/dialogarena/common/web/components/header/InnstillingerPanel.java
+++ web/src/main/java/no/nav/sbl/dialogarena/common/web/components/header/InnstillingerPanel.java
@@ -2,12 +2,18 @@ package no.nav.sbl.dialogarena.common.web.components.header;
 
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.StringResourceModel;
 
 public class InnstillingerPanel extends Panel {
 
     public InnstillingerPanel(String id) {
         super(id);
-        add(new Label("label", "{innstillinger}"));
+
+        add(
+                new Label("skriftstorrelse", new StringResourceModel("skriftstorrelse.label", this, null)),
+                new Label("hoykontrast", new StringResourceModel("hoykontrast.label", this, null))
+                );
+
     }
 
 }
