d6cdaecde77b092ec3a5ae14d949530b862b100a
Stian Landsnes <Stian.Landsnes@nav.no>
2013-04-05 09:55:20 +0000

    MOD-3600: ny headerkomponent, InnstillingerPanel
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/common/trunk@17278 6ec884bf-e345-0410-bd42-d282eb737ba0

--- src/main/java/no/nav/sbl/dialogarena/common/web/ShowcaseApplication.java
+++ src/main/java/no/nav/sbl/dialogarena/common/web/ShowcaseApplication.java
@@ -1,5 +1,8 @@
 package no.nav.sbl.dialogarena.common.web;
 
+import static no.nav.modig.frontend.FrontendModules.ALL;
+import no.nav.modig.frontend.FrontendConfigurator;
+import no.nav.modig.frontend.MetaTag;
 import no.nav.sbl.dialogarena.common.web.pages.ShowcasePage;
 
 import org.apache.wicket.Page;
@@ -10,6 +13,17 @@ public class ShowcaseApplication extends WebApplication {
     @Override
     public Class<? extends Page> getHomePage() {
         return ShowcasePage.class;
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+
+        new FrontendConfigurator()
+                .withModules(ALL)
+                .addMetas(MetaTag.CHARSET_UTF8, MetaTag.VIEWPORT_SCALE_1, MetaTag.XUA_IE_EDGE)
+                .withResourcePacking(this.usesDeploymentConfig())
+                .configure(this);
     }
 
 }
