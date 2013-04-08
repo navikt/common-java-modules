34594e96abf7cba00642382be04c54cad2de501a
Stian Landsnes <Stian.Landsnes@nav.no>
2013-04-08 13:55:59 +0000

    MOD-3600: ny headerkomponent, InnstillingerPanel
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/common/trunk@17403 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/main/java/no/nav/sbl/dialogarena/common/web/components/header/InnstillingerPanel.java
+++ web/src/main/java/no/nav/sbl/dialogarena/common/web/components/header/InnstillingerPanel.java
@@ -1,5 +1,7 @@
 package no.nav.sbl.dialogarena.common.web.components.header;
 
+import org.apache.wicket.Component;
+import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.ResourceModel;
@@ -9,12 +11,11 @@ public class InnstillingerPanel extends Panel {
     public InnstillingerPanel(String id) {
         super(id);
 
-        add(
-                new Label("skriftstorrelse_content", new ResourceModel("skriftstorrelse.content.label")),
-                new Label("skriftstorrelse", new ResourceModel("skriftstorrelse.label")),
-                new Label("hoykontrast", new ResourceModel("hoykontrast.label"))
-                );
+        Component skriftstorrelse = new Label("skriftstorrelse", new ResourceModel("skriftstorrelse.label"))
+                .add(AttributeAppender.append("title", new ResourceModel("skriftstorrelse.content.label")));
+        Component hoykontrast = new Label("hoykontrast", new ResourceModel("hoykontrast.label"));
 
+        add(skriftstorrelse, hoykontrast);
     }
 
 }
