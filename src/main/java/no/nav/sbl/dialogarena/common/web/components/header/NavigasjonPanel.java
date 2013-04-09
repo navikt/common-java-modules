8cb1b2f7ef2913d08c6808b1f400484a4136dc92
Stian Landsnes <Stian.Landsnes@nav.no>
2013-04-09 13:43:11 +0000

    MOD-3600: ny headerkomponent, InnstillingerPanel
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/common/trunk@17464 6ec884bf-e345-0410-bd42-d282eb737ba0

--- src/main/java/no/nav/sbl/dialogarena/common/web/components/header/NavigasjonPanel.java
+++ src/main/java/no/nav/sbl/dialogarena/common/web/components/header/NavigasjonPanel.java
@@ -1,13 +1,11 @@
 package no.nav.sbl.dialogarena.common.web.components.header;
 
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 
 public class NavigasjonPanel extends Panel {
 
     public NavigasjonPanel(String id) {
         super(id);
-        add(new Label("label", "{navigasjon}"));
     }
 
 }
