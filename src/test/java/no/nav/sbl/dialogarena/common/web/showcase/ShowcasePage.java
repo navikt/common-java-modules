7a169e34956ff9c2c45d8468f1174403f74b6742
Stian Landsnes <Stian.Landsnes@nav.no>
2013-04-10 09:07:30 +0000

    MOD-3600: flyttet felleskomponentene navigasjon og innstillinger til egne moduler
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/common/trunk@17490 6ec884bf-e345-0410-bd42-d282eb737ba0

--- src/test/java/no/nav/sbl/dialogarena/common/web/showcase/ShowcasePage.java
+++ src/test/java/no/nav/sbl/dialogarena/common/web/showcase/ShowcasePage.java
@@ -1,8 +1,9 @@
 package no.nav.sbl.dialogarena.common.web.showcase;
 
 import no.nav.sbl.dialogarena.common.footer.FooterPanel;
-import no.nav.sbl.dialogarena.common.web.components.header.InnstillingerPanel;
-import no.nav.sbl.dialogarena.common.web.components.header.NavigasjonPanel;
+import no.nav.sbl.dialogarena.common.innstillinger.InnstillingerPanel;
+import no.nav.sbl.dialogarena.common.navigasjon.NavigasjonPanel;
+
 import org.apache.wicket.markup.html.WebPage;
 
 public class ShowcasePage extends WebPage {
