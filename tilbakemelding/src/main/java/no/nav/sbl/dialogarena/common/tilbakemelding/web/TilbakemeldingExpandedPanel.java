1e67f1db6fa0379f83fc489d2defbc7dc5e4289f
Stian Landsnes <Stian.Landsnes@nav.no>
2013-04-16 10:31:53 +0000

    MOD-3604: GUI tilbakemeldinger komponent
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/common/trunk@17887 6ec884bf-e345-0410-bd42-d282eb737ba0

--- tilbakemelding/src/main/java/no/nav/sbl/dialogarena/common/tilbakemelding/web/TilbakemeldingExpandedPanel.java
+++ tilbakemelding/src/main/java/no/nav/sbl/dialogarena/common/tilbakemelding/web/TilbakemeldingExpandedPanel.java
@@ -1,9 +1,7 @@
 package no.nav.sbl.dialogarena.common.tilbakemelding.web;
 
-import no.nav.modig.wicket.conditional.ConditionalUtils;
 import no.nav.sbl.dialogarena.common.tilbakemelding.service.TilbakemeldingService;
 
-import org.apache.wicket.event.IEvent;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Button;
@@ -19,28 +17,14 @@ class TilbakemeldingExpandedPanel extends WebMarkupContainer {
 
     private TilbakemeldingService service;
 
-    public TilbakemeldingExpandedPanel(String id, IModel<Boolean> isVisible, TilbakemeldingService service) {
-        super(id, isVisible);
+    public TilbakemeldingExpandedPanel(String id, TilbakemeldingService service) {
+        super(id);
         this.service = service;
-
-        add(ConditionalUtils.visibleIf(isVisible));
 
         add(
                 new Label("label-tilbakemelding-expanded-header", new ResourceModel("label.tilbakemelding.expanded.header")),
                 new Label("label-tilbakemelding-expanded-content", new ResourceModel("label.tilbakemelding.expanded.content")),
                 new TilbakemeldingForm("form-tilbakemelding"));
-    }
-
-    @Override
-    public void onEvent(IEvent<?> event) {
-        if (event.getSource() instanceof TilbakemeldingPanel) {
-            changeVisibility();
-        }
-    }
-
-    private void changeVisibility() {
-        Boolean isVisible = (Boolean) getDefaultModelObject();
-        setDefaultModelObject(isVisible ? false : true);
     }
 
     private class TilbakemeldingForm extends Form<Void> {
@@ -53,7 +37,8 @@ class TilbakemeldingExpandedPanel extends WebMarkupContainer {
             add(
                     new Label("form-tilbakemelding-header", new ResourceModel("form.tilbakemelding.header")),
                     new TextField<>("form-tilbakemelding-input", tilbakemelding),
-                    new Button("form-tilbakemelding-submit", new ResourceModel("form.tilbakemelding.submit")));
+                    new Button("form-tilbakemelding-submit", new ResourceModel("form.tilbakemelding.submit")),
+                    new Label("form-tilbakemelding-abort", new ResourceModel("form.tilbakemelding.abort")));
         }
 
         @Override
--- tilbakemelding/src/main/java/no/nav/sbl/dialogarena/common/tilbakemelding/web/TilbakemeldingPanel.java
+++ tilbakemelding/src/main/java/no/nav/sbl/dialogarena/common/tilbakemelding/web/TilbakemeldingPanel.java
@@ -1,8 +1,5 @@
 package no.nav.sbl.dialogarena.common.tilbakemelding.web;
 
-import org.apache.wicket.ajax.AjaxEventBehavior;
-import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.event.Broadcast;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.ResourceModel;
@@ -18,14 +15,6 @@ class TilbakemeldingPanel extends WebMarkupContainer {
                 new Label("label-tilbakemelding-header", new ResourceModel("label.tilbakemelding.header")),
                 new Label("label-tilbakemelding-content", new ResourceModel("label.tilbakemelding.content"))
         );
-
-        add(new AjaxEventBehavior("click") {
-            @Override
-            protected void onEvent(AjaxRequestTarget target) {
-                target.add(getParent());
-                send(getParent(), Broadcast.DEPTH, null);
-            }
-        });
     }
 
 }
