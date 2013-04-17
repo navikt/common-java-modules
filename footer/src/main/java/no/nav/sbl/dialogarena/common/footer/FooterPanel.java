13148da7a3cdb1e8a0da02fc447539417ba435ed
Jarle Svendsrud <Jarle.Svendsrud@nav.no>
2013-04-17 08:26:31 +0000

    QA Footer + styling
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/common/trunk@17980 6ec884bf-e345-0410-bd42-d282eb737ba0

--- footer/src/main/java/no/nav/sbl/dialogarena/common/footer/FooterPanel.java
+++ footer/src/main/java/no/nav/sbl/dialogarena/common/footer/FooterPanel.java
@@ -6,15 +6,17 @@ import org.apache.wicket.model.Model;
 import org.apache.wicket.model.StringResourceModel;
 
 public class FooterPanel extends Panel {
+
     public FooterPanel(String id) {
         super(id);
         add(
-            new ExternalLink("kontakt", new Model<String>("#"), new StringResourceModel("footer.link.kontakt_oss", null)),
-            new ExternalLink("personvern", new Model<String>("#"), new StringResourceModel("footer.link.personvern_og_sikkerhet", null)),
-            new ExternalLink("feil", new Model<String>("#"), new StringResourceModel("footer.link.feil_og_mangler", null)),
-            new ExternalLink("tilgjengelighet", new Model<String>("#"), new StringResourceModel("footer.link.tilgjengelighet", null)),
-            new ExternalLink("nyheter", new Model<String>("#"), new StringResourceModel("footer.link.nyheter_om_rss", null)),
-            new ExternalLink("nettstedskart", new Model<String>("#"), new StringResourceModel("footer.link.nettstedskart", null))
+            new ExternalLink("kontakt", new Model<>("#"), new StringResourceModel("footer.link.kontakt_oss", null)),
+            new ExternalLink("personvern", new Model<>("#"), new StringResourceModel("footer.link.personvern_og_sikkerhet", null)),
+            new ExternalLink("feil", new Model<>("#"), new StringResourceModel("footer.link.feil_og_mangler", null)),
+            new ExternalLink("tilgjengelighet", new Model<>("#"), new StringResourceModel("footer.link.tilgjengelighet", null)),
+            new ExternalLink("nyheter", new Model<>("#"), new StringResourceModel("footer.link.nyheter_om_rss", null)),
+            new ExternalLink("nettstedskart", new Model<>("#"), new StringResourceModel("footer.link.nettstedskart", null))
         );
     }
+
 }
