b1d8e48c0553fb0e8cfb78fecbddd1abc39c631b
Stian Landsnes <Stian.Landsnes@nav.no>
2013-04-18 09:58:34 +0000

    MOD-3604: GUI tilbakemeldinger komponent
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/common/trunk@18107 6ec884bf-e345-0410-bd42-d282eb737ba0

--- tilbakemelding/src/main/java/no/nav/sbl/dialogarena/common/tilbakemelding/web/TilbakemeldingContainer.java
+++ tilbakemelding/src/main/java/no/nav/sbl/dialogarena/common/tilbakemelding/web/TilbakemeldingContainer.java
@@ -1,6 +1,7 @@
 package no.nav.sbl.dialogarena.common.tilbakemelding.web;
 
 import no.nav.sbl.dialogarena.common.tilbakemelding.service.TilbakemeldingService;
+
 import org.apache.wicket.markup.html.panel.Panel;
 
 public class TilbakemeldingContainer extends Panel {
@@ -8,9 +9,11 @@ public class TilbakemeldingContainer extends Panel {
     public TilbakemeldingContainer(String id, String applikasjonsnavn, TilbakemeldingService service) {
         super(id);
 
-        add(
-                new TilbakemeldingPanel("panel-tilbakemelding", applikasjonsnavn),
-                new TilbakemeldingExpandedPanel("panel-tilbakemelding-expanded", service));
+        final TilbakemeldingPanel tilbakemelding = new TilbakemeldingPanel("panel-tilbakemelding", applikasjonsnavn);
+        final TilbakemeldingExpandedPanel tilbakemeldingExpanded = new TilbakemeldingExpandedPanel(
+                "panel-tilbakemelding-expanded", service, tilbakemelding);
+
+        add(tilbakemelding, tilbakemeldingExpanded);
     }
 
 }
--- tilbakemelding/src/main/java/no/nav/sbl/dialogarena/common/tilbakemelding/web/TilbakemeldingExpandedPanel.java
+++ tilbakemelding/src/main/java/no/nav/sbl/dialogarena/common/tilbakemelding/web/TilbakemeldingExpandedPanel.java
@@ -2,48 +2,66 @@ package no.nav.sbl.dialogarena.common.tilbakemelding.web;
 
 import no.nav.sbl.dialogarena.common.tilbakemelding.service.TilbakemeldingService;
 
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.form.TextArea;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.ResourceModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class TilbakemeldingExpandedPanel extends WebMarkupContainer {
 
     private static final long serialVersionUID = 1L;
 
-    private TilbakemeldingService service;
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    public TilbakemeldingExpandedPanel(String id, TilbakemeldingService service) {
+    private TilbakemeldingService service;
+    private TilbakemeldingPanel tilbakemeldingPanel;
+
+    public TilbakemeldingExpandedPanel(String id, TilbakemeldingService service, TilbakemeldingPanel tilbakemeldingPanel) {
         super(id);
         this.service = service;
+        this.tilbakemeldingPanel = tilbakemeldingPanel;
 
         add(
                 new Label("label-tilbakemelding-expanded-header", new ResourceModel("label.tilbakemelding.expanded.header")),
                 new Label("label-tilbakemelding-expanded-content", new ResourceModel("label.tilbakemelding.expanded.content")),
-                new TilbakemeldingForm("form-tilbakemelding"));
+                new TilbakemeldingForm("form-tilbakemelding", Model.of(new String())));
     }
 
-    private class TilbakemeldingForm extends Form<Void> {
+    private class TilbakemeldingForm extends Form<String> {
 
-        private IModel<String> tilbakemelding = new Model<>();
+        public TilbakemeldingForm(String id, IModel<String> tilbakemelding) {
+            super(id, tilbakemelding);
 
-        public TilbakemeldingForm(String id) {
-            super(id);
+            AjaxButton ajaxButton = new AjaxButton("form-tilbakemelding-submit", this) {
+
+                @Override
+                protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+                    boolean success = true;
+                    try {
+                        service.sendTilbakemelding((String) form.getDefaultModelObject());
+                    } catch (Exception e) {
+                        logger.error("Error while sending tilbakemelding", e);
+                        success = false;
+                    }
+                    target.add(tilbakemeldingPanel);
+                    tilbakemeldingPanel.success(success);
+                }
+
+            };
 
             add(
+                    ajaxButton,
                     new Label("form-tilbakemelding-header", new ResourceModel("form.tilbakemelding.header")),
-                    new TextField<>("form-tilbakemelding-input", tilbakemelding),
-                    new Button("form-tilbakemelding-submit", new ResourceModel("form.tilbakemelding.submit")),
+                    new TextArea<>("form-tilbakemelding-input", tilbakemelding),
                     new Label("form-tilbakemelding-abort", new ResourceModel("form.tilbakemelding.abort")));
-        }
 
-        @Override
-        protected void onSubmit() {
-            service.sendTilbakemelding(tilbakemelding.getObject());
         }
 
     }
