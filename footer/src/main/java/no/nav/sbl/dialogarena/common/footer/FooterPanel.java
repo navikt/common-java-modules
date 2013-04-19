a84671465240ea27d0d3f3116391af1bac1dd438
Håkon Haga <Hakon.Haga@nav.no>
2013-04-19 11:27:54 +0000

    MOD-3602 årstall dynamisk i footer
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/common/trunk@18238 6ec884bf-e345-0410-bd42-d282eb737ba0

--- footer/src/main/java/no/nav/sbl/dialogarena/common/footer/FooterPanel.java
+++ footer/src/main/java/no/nav/sbl/dialogarena/common/footer/FooterPanel.java
@@ -1,9 +1,12 @@
 package no.nav.sbl.dialogarena.common.footer;
 
+import org.apache.wicket.datetime.markup.html.basic.DateLabel;
 import org.apache.wicket.markup.html.link.ExternalLink;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.StringResourceModel;
+
+import java.util.Date;
 
 public class FooterPanel extends Panel {
 
@@ -14,7 +17,8 @@ public class FooterPanel extends Panel {
             new ExternalLink("personvern", new Model<>("#"), new StringResourceModel("footer.link.personvern_og_sikkerhet", null)),
             new ExternalLink("feil", new Model<>("#"), new StringResourceModel("footer.link.feil_og_mangler", null)),
             new ExternalLink("tilgjengelighet", new Model<>("#"), new StringResourceModel("footer.link.tilgjengelighet", null)),
-            new ExternalLink("nettstedskart", new Model<>("#"), new StringResourceModel("footer.link.nettstedskart", null))
+            new ExternalLink("nettstedskart", new Model<>("#"), new StringResourceModel("footer.link.nettstedskart", null)),
+            DateLabel.forDatePattern("year", Model.of(new Date()), "yyyy")
         );
     }
 
