46bcc8d546ddfeecd018fbf7c6a8558589ca3036
Stian Landsnes <Stian.Landsnes@nav.no>
2013-04-23 12:32:16 +0000

    MOD-3598, 3600: Endringer InnstillingerPanel
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/common/trunk@18388 6ec884bf-e345-0410-bd42-d282eb737ba0

--- innstillinger/src/main/java/no/nav/sbl/dialogarena/common/innstillinger/InnstillingerPanel.java
+++ innstillinger/src/main/java/no/nav/sbl/dialogarena/common/innstillinger/InnstillingerPanel.java
@@ -1,11 +1,12 @@
 package no.nav.sbl.dialogarena.common.innstillinger;
 
-import org.apache.wicket.behavior.AttributeAppender;
+import static no.nav.modig.wicket.conditional.ConditionalUtils.hasCssClassIf;
+import no.nav.modig.core.context.SecurityContext;
+
 import org.apache.wicket.markup.head.CssContentHeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.JavaScriptContentHeaderItem;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.request.resource.CssResourceReference;
 import org.apache.wicket.request.resource.JavaScriptResourceReference;
@@ -18,25 +19,27 @@ public class InnstillingerPanel extends Panel {
     private static final CssResourceReference INNSTILLINGER_PANEL_CSS = new CssResourceReference(
             InnstillingerPanel.class, "InnstillingerPanel.css");
 
-    private static final String INNLOGGET_STYLE = "innlogget";
-    private static final String IKKE_INNLOGGET_STYLE = "";
-
-    private IModel<String> isInnloggetModel = Model.of(IKKE_INNLOGGET_STYLE);
+    private static final String INNLOGGET_CSS_CLASS = "innlogget";
 
     public InnstillingerPanel(String id) {
         super(id);
-        setOutputMarkupPlaceholderTag(true);
-        add(new AttributeAppender("class", isInnloggetModel));
-    }
+        setOutputMarkupId(true);
 
-    public final InnstillingerPanel setInnlogget(boolean isInnlogget) {
-        isInnloggetModel.setObject(isInnlogget ? INNLOGGET_STYLE : IKKE_INNLOGGET_STYLE);
-        return this;
+        add(hasCssClassIf(INNLOGGET_CSS_CLASS, new IsInnlogget()));
     }
 
     @Override
-    public final void renderHead(IHeaderResponse response) {
+    public void renderHead(IHeaderResponse response) {
         response.render(JavaScriptContentHeaderItem.forReference(INNSTILLINGER_PANEL_JS));
         response.render(CssContentHeaderItem.forReference(INNSTILLINGER_PANEL_CSS));
     }
+
+    private class IsInnlogget extends Model<Boolean> {
+
+        public IsInnlogget() {
+            super(SecurityContext.getCurrent().getPrincipal() == null ? false : true);
+        }
+
+    }
+
 }
