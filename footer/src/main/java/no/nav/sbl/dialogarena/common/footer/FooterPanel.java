0eaad970757a29bf0157f7589d93de2a19bdd0d2
Håkon Haga <Hakon.Haga@nav.no>
2013-04-24 12:07:24 +0000

    MOD-3602 lagt til konfigurasjon av URL i footer
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/common/trunk@18470 6ec884bf-e345-0410-bd42-d282eb737ba0

--- footer/src/main/java/no/nav/sbl/dialogarena/common/footer/FooterPanel.java
+++ footer/src/main/java/no/nav/sbl/dialogarena/common/footer/FooterPanel.java
@@ -7,17 +7,29 @@ import org.apache.wicket.model.Model;
 import org.apache.wicket.model.StringResourceModel;
 
 import java.util.Date;
+import java.util.Map;
 
+/**
+ * Felleskomponent som bl.a. brukes på forside i inngangsporten.
+ *
+ * Statiske public fields gjør det mulig å sikre at nøkkel er lik ved innstansiering
+ *
+ */
 public class FooterPanel extends Panel {
+    public static final String FOOTER_KONTAKT_URL = "footer.kontakt.url";
+    public static final String FOOTER_PERSONVERN_URL = "footer.personvern.url";
+    public static final String FOOTER_FEIL_OG_MANGLER_URL = "footer.feilOgMangler.url";
+    public static final String FOOTER_TILGJENGELIGHET_URL = "footer.tilgjengelighet.url";
+    public static final String FOOTER_NETTSTEDSKART_URL = "footer.nettstedskart.url";
 
-    public FooterPanel(String id) {
+    public FooterPanel(String id, Map<String, String> links) {
         super(id);
         add(
-            new ExternalLink("kontakt", new Model<>("#"), new StringResourceModel("footer.link.kontakt_oss", null)),
-            new ExternalLink("personvern", new Model<>("#"), new StringResourceModel("footer.link.personvern_og_sikkerhet", null)),
-            new ExternalLink("feil", new Model<>("#"), new StringResourceModel("footer.link.feil_og_mangler", null)),
-            new ExternalLink("tilgjengelighet", new Model<>("#"), new StringResourceModel("footer.link.tilgjengelighet", null)),
-            new ExternalLink("nettstedskart", new Model<>("#"), new StringResourceModel("footer.link.nettstedskart", null)),
+            new ExternalLink("kontakt", new Model<>(links.get(FOOTER_KONTAKT_URL)), new StringResourceModel("footer.link.kontakt_oss", null)),
+            new ExternalLink("personvern", new Model<>(links.get(FOOTER_PERSONVERN_URL)), new StringResourceModel("footer.link.personvern_og_sikkerhet", null)),
+            new ExternalLink("feil", new Model<>(links.get(FOOTER_FEIL_OG_MANGLER_URL)), new StringResourceModel("footer.link.feil_og_mangler", null)),
+            new ExternalLink("tilgjengelighet", new Model<>(links.get(FOOTER_TILGJENGELIGHET_URL)), new StringResourceModel("footer.link.tilgjengelighet", null)),
+            new ExternalLink("nettstedskart", new Model<>(links.get(FOOTER_NETTSTEDSKART_URL)), new StringResourceModel("footer.link.nettstedskart", null)),
             DateLabel.forDatePattern("year", Model.of(new Date()), "yyyy")
         );
     }
