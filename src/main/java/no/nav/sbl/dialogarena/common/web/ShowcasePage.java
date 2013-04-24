963bc9779a6520debd071e3a9e613da539f0cbac
Stian Landsnes <Stian.Landsnes@nav.no>
2013-04-24 14:17:07 +0000

    MOD-3602, QC-193: Visning av link nettstedskart
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/common/trunk@18497 6ec884bf-e345-0410-bd42-d282eb737ba0

--- src/main/java/no/nav/sbl/dialogarena/common/web/ShowcasePage.java
+++ src/main/java/no/nav/sbl/dialogarena/common/web/ShowcasePage.java
@@ -1,16 +1,21 @@
 package no.nav.sbl.dialogarena.common.web;
 
+import java.util.HashMap;
+
 import no.nav.sbl.dialogarena.common.footer.FooterPanel;
 import no.nav.sbl.dialogarena.common.innstillinger.InnstillingerPanel;
 import no.nav.sbl.dialogarena.common.navigasjon.NavigasjonPanel;
 import no.nav.sbl.dialogarena.common.tilbakemelding.service.TilbakemeldingService;
 import no.nav.sbl.dialogarena.common.tilbakemelding.web.TilbakemeldingContainer;
+
 import org.apache.wicket.Component;
+import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.link.InlineFrame;
 import org.apache.wicket.model.AbstractReadOnlyModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
@@ -24,6 +29,8 @@ public class ShowcasePage extends WebPage {
     private ContentPage contentPage = new ContentPage();
     private InlineFrame inlineFrame = new InlineFrame("iframe", contentPage);
 
+    public static boolean isInnlogget = false;
+
     public ShowcasePage(PageParameters parameters) {
         super(parameters);
 
@@ -31,16 +38,28 @@ public class ShowcasePage extends WebPage {
 
         add(inlineFrame);
 
+        add(new AjaxLink<Void>("trigger-innlogging") {
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                isInnlogget = isInnlogget ? false : true;
+                target.add(inlineFrame, this);
+            }
+        });
+
         addShowcaseComponent(
-                new InnstillingerPanel("innstillinger", new AbstractReadOnlyModel<Boolean>() {
-                    @Override
-                    public Boolean getObject() {
-                        return false;
-                    }
-                }),
+                new InnstillingerPanel("innstillinger", new IsInnlogget()),
                 new NavigasjonPanel("navigasjon", "www.nav.no"),
-                new FooterPanel("footer", new HashMap<String, String>()),
+                new FooterPanel("footer", new HashMap<String, String>(), new IsInnlogget()),
                 new TilbakemeldingContainer("tilbakemelding", "Din profil", tilbakemeldingService));
+    }
+
+    private class IsInnlogget extends AbstractReadOnlyModel<Boolean> {
+
+        @Override
+        public Boolean getObject() {
+            return isInnlogget;
+        }
+
     }
 
     private void addShowcaseComponent(Component... components) {
