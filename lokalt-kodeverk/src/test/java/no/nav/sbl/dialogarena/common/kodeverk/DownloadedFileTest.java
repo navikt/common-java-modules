871db8141f521c57baab4b28a977d9ca93953f66
Øyvind Norsveen <Oyvind.Nordsveen@nav.no>
2013-05-27 08:04:38 +0000

    Refaktorering av bygg, for å få det til å bygge på jenkins
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/common/trunk@27167 6ec884bf-e345-0410-bd42-d282eb737ba0

--- lokalt-kodeverk/src/test/java/no/nav/sbl/dialogarena/common/kodeverk/DownloadedFileTest.java
+++ lokalt-kodeverk/src/test/java/no/nav/sbl/dialogarena/common/kodeverk/DownloadedFileTest.java
@@ -7,24 +7,11 @@ import java.io.FileNotFoundException;
 
 public class DownloadedFileTest {
 
-    private static final String JSONFILE_LOCATION_IDE = "lokalt-kodeverk/target/classes/kodeverk.json";
-    private static final String JSONFILE_LOCATION_MAVEN = "target/classes/kodeverk.json";
-
     @Test
     public void shouldParseDownloadedFile() throws FileNotFoundException {
         FileInputStream jsonFile;
-        // Prøver å åpne fil slik IDE ser den først. Hvis det ikke fungerer, forsøk slik Maven ser den.
-        try {
-            jsonFile = new FileInputStream(JSONFILE_LOCATION_IDE);
-        } catch (FileNotFoundException ex) {
-            try {
-                jsonFile = new FileInputStream(JSONFILE_LOCATION_MAVEN);
-            } catch (FileNotFoundException ex2) {
-                throw ex2;
-            }
-        }
         System.out.println("Trying to parse new kodeverk");
-        new JsonKodeverk(jsonFile);
+        new JsonKodeverk(DownloadedFileTest.class.getResourceAsStream("/kodeverk.json"));
         System.out.println("Kodeverk OK!");
     }
 }
