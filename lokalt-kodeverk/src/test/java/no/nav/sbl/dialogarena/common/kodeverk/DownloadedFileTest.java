793f6677791654f10de6c3f004d3b7eb8e6d860a
Øyvind Norsveen <Oyvind.Nordsveen@nav.no>
2013-05-27 08:20:58 +0000

    Fikser bygg.
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/common/trunk@27169 6ec884bf-e345-0410-bd42-d282eb737ba0

--- lokalt-kodeverk/src/test/java/no/nav/sbl/dialogarena/common/kodeverk/DownloadedFileTest.java
+++ lokalt-kodeverk/src/test/java/no/nav/sbl/dialogarena/common/kodeverk/DownloadedFileTest.java
@@ -2,14 +2,13 @@ package no.nav.sbl.dialogarena.common.kodeverk;
 
 import org.junit.Test;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class DownloadedFileTest {
 
     @Test
+    @SuppressWarnings("PMD.SystemPrintln")
     public void shouldParseDownloadedFile() throws FileNotFoundException {
-        FileInputStream jsonFile;
         System.out.println("Trying to parse new kodeverk");
         new JsonKodeverk(DownloadedFileTest.class.getResourceAsStream("/kodeverk.json"));
         System.out.println("Kodeverk OK!");
