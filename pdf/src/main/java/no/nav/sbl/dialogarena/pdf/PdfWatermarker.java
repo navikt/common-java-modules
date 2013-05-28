a353768e07d112dd2893a62585ee87b40ac7823e
Thomas Fossum <Thomas.Fossum@nav.no>
2013-05-28 13:41:21 +0000

    MOD-4227 Vannmerking av PDF gjøres nå med PDFBox
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/common/trunk@27350 6ec884bf-e345-0410-bd42-d282eb737ba0

--- pdf/src/main/java/no/nav/sbl/dialogarena/pdf/PdfWatermarker.java
+++ pdf/src/main/java/no/nav/sbl/dialogarena/pdf/PdfWatermarker.java
@@ -1,33 +1,32 @@
 package no.nav.sbl.dialogarena.pdf;
 
-import com.itextpdf.text.BaseColor;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Rectangle;
-import com.itextpdf.text.pdf.BaseFont;
-import com.itextpdf.text.pdf.PdfContentByte;
-import com.itextpdf.text.pdf.PdfGState;
-import com.itextpdf.text.pdf.PdfReader;
-import com.itextpdf.text.pdf.PdfStamper;
 import no.nav.sbl.dialogarena.detect.IsPdf;
 import org.apache.commons.collections15.Transformer;
+import org.apache.pdfbox.exceptions.COSVisitorException;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDResources;
+import org.apache.pdfbox.pdmodel.edit.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.graphics.PDExtendedGraphicsState;
 import org.joda.time.DateTime;
 
+import java.awt.*;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
-import static com.itextpdf.text.BaseColor.BLACK;
-import static com.itextpdf.text.BaseColor.RED;
-import static com.itextpdf.text.BaseColor.WHITE;
-import static com.itextpdf.text.pdf.BaseFont.EMBEDDED;
-import static com.itextpdf.text.pdf.BaseFont.HELVETICA;
-import static com.itextpdf.text.pdf.BaseFont.WINANSI;
-import static com.itextpdf.text.pdf.PdfContentByte.ALIGN_LEFT;
 import static java.lang.Math.max;
 
 /**
  * Vannmerker PDF
- *
+ * <p/>
  * For flersidig PDF legges vannmerket på samtlige sider.
  */
 public class PdfWatermarker {
@@ -39,12 +38,13 @@ public class PdfWatermarker {
     private static final int PADDING_X = 5;
     private static final int PADDING_Y = 5;
     private static final int LINE_SPACING = 5;
-
-    private static final BaseColor BACKGROUND_COLOR = WHITE;
-    private static final float BACKGROUND_OPACITY = 0.9f;
-    private static final BaseColor TEXT_COLOR = BLACK;
-    private static final BaseColor BORDER_COLOR = RED;
+    private static final int LINJE1_START_Y = 13;
+    private static final int LINJE2_START_Y = 5;
+    private static final float BACKGROUND_OPACITY = 0.8f;
+    private static final float DEFAULT_BACKGROUND_OPACITY = 1.0f;
     private static final float BORDER_WIDTH = 1f;
+    private static final float HEIGHT = 22f;
+    private static final PDFont FONT = PDType1Font.HELVETICA;
     private static final int FONT_SIZE = 6;
 
 
@@ -53,104 +53,37 @@ public class PdfWatermarker {
     }
 
     public final byte[] applyOn(byte[] bytes, String ident) {
+
+        PDDocument pdfDocument = null;
+        String linje1 = LINE_1_HEADER + formatertDato();
+        String linje2 = LINE_2_HEADER + ident;
+
         if (!(new IsPdf().evaluate(bytes))) {
             throw new IllegalArgumentException("Kan kun vannmerke PDF-filer.");
         }
 
-        String linje1 = LINE_1_HEADER + formatertDato();
-        String linje2 = LINE_2_HEADER + ident;
-
-        byte[] watermarkedPdf;
         try {
-            PdfReader originalReader = new PdfReader(bytes);
-            byte[] rectangleStampedPdf = stampRectangleOnPdf(originalReader, linje1, linje2);
-            PdfReader rectangleReader = new PdfReader(rectangleStampedPdf);
-            watermarkedPdf = stampTextOnPdf(rectangleReader, linje1, linje2);
-        } catch (DocumentException | IOException e) {
+            InputStream inputStream = new ByteArrayInputStream(bytes);
+            pdfDocument = PDDocument.load(inputStream);
+
+            stampRectangleOnPdf(pdfDocument, linje1, linje2);
+            stampTextOnPdf(pdfDocument, linje1, linje2);
+
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            pdfDocument.save(output);
+            return output.toByteArray();
+        } catch (COSVisitorException | IOException e) {
             throw new RuntimeException(e.getMessage(), e);
+        } finally {
+            try {
+                if (pdfDocument != null) {
+                    pdfDocument.close();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e.getMessage(), e);
+            }
         }
-        return watermarkedPdf;
     }
-
-
-    private byte[] stampRectangleOnPdf(PdfReader reader, String linje1, String linje2) throws IOException, DocumentException {
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        PdfStamper stamper = new PdfStamper(reader, byteArrayOutputStream);
-
-        for (int i = 1; i <= reader.getNumberOfPages(); i++) {
-            float pageWidth = reader.getPageSize(i).getWidth();
-            float pageHeigth = reader.getPageSize(i).getHeight();
-
-            PdfContentByte rectangleContent = stamper.getOverContent(i);
-            rectangleContent.setColorStroke(TEXT_COLOR);
-            rectangleContent.setFontAndSize(createFont(), FONT_SIZE);
-
-            float lineWidth = max(rectangleContent.getEffectiveStringWidth(linje1, false),
-                    rectangleContent.getEffectiveStringWidth(linje2, false));
-
-            PdfGState gstate = new PdfGState();
-            gstate.setFillOpacity(BACKGROUND_OPACITY);
-            rectangleContent.setGState(gstate);
-            rectangleContent.rectangle(watermarkFrame(pageWidth, pageHeigth, lineWidth));
-
-        }
-        stamper.close();
-        return byteArrayOutputStream.toByteArray();
-    }
-
-    private byte[] stampTextOnPdf(PdfReader reader, String linje1, String linje2) throws IOException, DocumentException {
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        PdfStamper stamper = new PdfStamper(reader, byteArrayOutputStream);
-
-        for (int i = 1; i <= reader.getNumberOfPages(); i++) {
-            float pageWidth = reader.getPageSize(i).getWidth();
-            float pageHeigth = reader.getPageSize(i).getHeight();
-
-            PdfContentByte textContent = stamper.getOverContent(i);
-            textContent.setColorStroke(TEXT_COLOR);
-            textContent.setFontAndSize(createFont(), FONT_SIZE);
-
-            float lineWidth = max(textContent.getEffectiveStringWidth(linje1, false),
-                    textContent.getEffectiveStringWidth(linje2, false));
-
-            float magicNumber = 2f; // Dette trengs for å få riktig vertikal offset
-            float textX = pageWidth - MARGIN - lineWidth - PADDING_X;
-            float linje1Y = pageHeigth - MARGIN - BORDER_WIDTH - PADDING_Y - FONT_SIZE / 2 - magicNumber;
-            float linje2Y = linje1Y - FONT_SIZE - LINE_SPACING;
-
-            textContent.beginText();
-            textContent.showTextAligned(ALIGN_LEFT, linje1, textX, linje1Y, 0);
-            textContent.showTextAligned(ALIGN_LEFT, linje2, textX, linje2Y, 0);
-            textContent.endText();
-        }
-        stamper.close();
-        return byteArrayOutputStream.toByteArray();
-    }
-
-    private String formatertDato() {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.YYYY', kl. 'HH:mm:ss");
-        return simpleDateFormat.format(DateTime.now().toDate());
-    }
-
-    private Rectangle watermarkFrame(float pageWidth, float pageHeigth, float lineWidth) {
-        float upperRightX = pageWidth - MARGIN;
-        float upperRightY = pageHeigth - MARGIN;
-        float lowerLeftX = upperRightX - lineWidth - 2 * PADDING_X;
-        float lowerLeftY = upperRightY - 2 * FONT_SIZE - 2 * PADDING_Y - LINE_SPACING - 2 * BORDER_WIDTH;
-
-        Rectangle frame = new Rectangle(lowerLeftX, lowerLeftY, upperRightX, upperRightY);
-        frame.setBackgroundColor(BACKGROUND_COLOR);
-        frame.enableBorderSide(Rectangle.BOX);
-        frame.setBorderColor(BORDER_COLOR);
-        frame.setBorderWidth(BORDER_WIDTH);
-        return frame;
-    }
-
-    private BaseFont createFont() throws DocumentException, IOException {
-        return BaseFont.createFont(HELVETICA, WINANSI, EMBEDDED);
-    }
-
-
 
     private class WatermarkerForIdent implements Transformer<byte[], byte[]> {
         private final String ident;
@@ -164,5 +97,101 @@ public class PdfWatermarker {
             return PdfWatermarker.this.applyOn(bytes, ident);
         }
     }
+
+    private void stampRectangleOnPdf(PDDocument pdfDocument, String linje1, String linje2) throws IOException {
+        List allPages = pdfDocument.getDocumentCatalog().getAllPages();
+        for (Object singlePage : allPages) {
+            PDPage page = (PDPage) singlePage;
+
+            float pageHeight = page.getMediaBox().getHeight();
+            float pageWidth = page.getMediaBox().getWidth();
+            float lineWidth = max(findLineWidthForTextWithFontSize(linje1, FONT, FONT_SIZE),
+                    findLineWidthForTextWithFontSize(linje2, FONT, FONT_SIZE)) + PADDING_X + PADDING_Y;
+
+            float upperRightX = pageWidth - MARGIN;
+            float upperRightY = pageHeight - MARGIN;
+            float lowerLeftX = upperRightX - lineWidth - 2 * PADDING_X;
+            float lowerLeftY = upperRightY - 2 * FONT_SIZE - 2 * PADDING_Y - LINE_SPACING - 2 * BORDER_WIDTH;
+
+            String key = "MyGraphicsState1";
+            setOpacity(page.findResources(), BACKGROUND_OPACITY, key);
+            PDPageContentStream contentStream = new PDPageContentStream(pdfDocument, page, true, true, true);
+            try {
+                contentStream.appendRawCommands("/" + key + " gs\n");
+                contentStream.setNonStrokingColor(Color.white);
+                contentStream.fillRect(lowerLeftX, lowerLeftY, lineWidth, HEIGHT);
+            } finally {
+                contentStream.close();
+            }
+        }
+    }
+
+    private void stampTextOnPdf(PDDocument pdfDocument, String linje1, String linje2) throws IOException {
+        List allPages = pdfDocument.getDocumentCatalog().getAllPages();
+        for (Object singlePage : allPages) {
+            PDPage page = (PDPage) singlePage;
+
+            float pageHeight = page.getMediaBox().getHeight();
+            float pageWidth = page.getMediaBox().getWidth();
+            float lineWidth = max(findLineWidthForTextWithFontSize(linje1, FONT, FONT_SIZE),
+                    findLineWidthForTextWithFontSize(linje2, FONT, FONT_SIZE)) + PADDING_X + PADDING_Y;
+
+            float upperRightX = pageWidth - MARGIN;
+            float upperRightY = pageHeight - MARGIN;
+            float lowerLeftX = upperRightX - lineWidth - 2 * PADDING_X;
+            float lowerLeftY = upperRightY - 2 * FONT_SIZE - 2 * PADDING_Y - LINE_SPACING - 2 * BORDER_WIDTH;
+
+            String key = "MyGraphicsState2";
+            setOpacity(page.findResources(), DEFAULT_BACKGROUND_OPACITY, key);
+
+            PDPageContentStream contentStream = new PDPageContentStream(pdfDocument, page, true, true, true);
+            try {
+                contentStream.appendRawCommands("/" + key + " gs\n");
+                contentStream.setFont(FONT, FONT_SIZE);
+                contentStream.setNonStrokingColor(Color.black);
+
+                contentStream.beginText();
+                contentStream.moveTextPositionByAmount(lowerLeftX + MARGIN, lowerLeftY + LINJE1_START_Y);
+                contentStream.drawString(linje1);
+                contentStream.endText();
+
+                contentStream.beginText();
+                contentStream.moveTextPositionByAmount(lowerLeftX + MARGIN, lowerLeftY + LINJE2_START_Y);
+                contentStream.drawString(linje2);
+                contentStream.endText();
+
+                contentStream.setStrokingColor(Color.red);
+                contentStream.setLineWidth(BORDER_WIDTH);
+                contentStream.drawLine(lowerLeftX, lowerLeftY, lowerLeftX, lowerLeftY + HEIGHT);
+                contentStream.drawLine(lowerLeftX, lowerLeftY + HEIGHT, lowerLeftX + lineWidth, lowerLeftY + HEIGHT);
+                contentStream.drawLine(lowerLeftX + lineWidth, lowerLeftY + HEIGHT, lowerLeftX + lineWidth, lowerLeftY);
+                contentStream.drawLine(lowerLeftX + lineWidth, lowerLeftY, lowerLeftX, lowerLeftY);
+            } finally {
+                contentStream.close();
+            }
+
+        }
+    }
+
+    private void setOpacity(PDResources resources, float opacity, String key) {
+        PDExtendedGraphicsState graphicsState = new PDExtendedGraphicsState();
+        graphicsState.setNonStrokingAlphaConstant(opacity);
+        Map<String, PDExtendedGraphicsState> graphicsStateDictionary = resources.getGraphicsStates();
+        if (graphicsStateDictionary == null) {
+            graphicsStateDictionary = new TreeMap<>();
+        }
+        graphicsStateDictionary.put(key, graphicsState);
+        resources.setGraphicsStates(graphicsStateDictionary);
+    }
+
+    private String formatertDato() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.YYYY', kl. 'HH:mm:ss");
+        return simpleDateFormat.format(DateTime.now().toDate());
+    }
+
+    private float findLineWidthForTextWithFontSize(String text, PDFont font, int fontSize) throws IOException {
+        return font.getStringWidth(text) / 1000 * fontSize;
+    }
+
 }
 
--- pdf/src/test/java/no/nav/sbl/dialogarena/pdf/PdfWatermarkerTest.java
+++ pdf/src/test/java/no/nav/sbl/dialogarena/pdf/PdfWatermarkerTest.java
@@ -1,15 +1,14 @@
 package no.nav.sbl.dialogarena.pdf;
 
-import com.itextpdf.text.pdf.PdfReader;
-import com.itextpdf.text.pdf.parser.PdfReaderContentParser;
-import com.itextpdf.text.pdf.parser.SimpleTextExtractionStrategy;
-import com.itextpdf.text.pdf.parser.TextExtractionStrategy;
 import no.nav.sbl.dialogarena.detect.IsPdf;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.PDFTextStripper;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeUtils;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 import static no.nav.sbl.dialogarena.pdf.PdfTestUtils.getBytesFromFile;
@@ -21,7 +20,7 @@ import static org.junit.Assert.assertThat;
 
 public class PdfWatermarkerTest {
 
-    private static final String MOCK_FODSELSNUMMER = "112233 12345";
+    private static final String MOCK_FODSELSNUMMER = "112233 99999";
     private static final String INPUT_PDF = "skjema";
 
     private final DateTime time = new DateTime(2013, 3, 18, 14, 30, 30);
@@ -35,29 +34,42 @@ public class PdfWatermarkerTest {
 
     @Test
     public void vannmerkerPdf() throws IOException {
-    	byte[] fileBytes = getBytesFromFile("/WatermarkerFiles/" + INPUT_PDF + ".pdf");
-
+        byte[] fileBytes = getBytesFromFile("/WatermarkerFiles/" + INPUT_PDF + ".pdf");
         byte[] watermarkedBytes = watermarker.forIdent(MOCK_FODSELSNUMMER).transform(fileBytes);
+
         assertThat(watermarkedBytes, match(new IsPdf()));
         assertThat(watermarkedBytes, not(fileBytes));
 
-        // Sjekk at hver side i vannmerket PDF inneholder tekst fra vannmerket
-        PdfReader reader = new PdfReader(watermarkedBytes);
-        PdfReaderContentParser parser = new PdfReaderContentParser(reader);
-        TextExtractionStrategy strategy;
-        for (int i = 1; i <= reader.getNumberOfPages(); i++) {
-            strategy = parser.processContent(i, new SimpleTextExtractionStrategy());
-            String pageText = strategy.getResultantText();
-            assertThat(pageText, containsString(PdfWatermarker.LINE_1_HEADER));
-            assertThat(pageText, containsString(PdfWatermarker.LINE_2_HEADER));
-            assertThat(pageText, containsString(MOCK_FODSELSNUMMER));
-            assertThat(pageText, containsString("18.03.2013, kl. 14:30:30"));
-        }
+        String pdfText = extractText(watermarkedBytes);
+        assertThat(pdfText, containsString(PdfWatermarker.LINE_1_HEADER));
+        assertThat(pdfText, containsString(PdfWatermarker.LINE_2_HEADER));
+        assertThat(pdfText, containsString(MOCK_FODSELSNUMMER));
+        assertThat(pdfText, containsString("18.03.2013, kl. 14:30:30"));
 
         writeBytesToFile(watermarkedBytes, "/WatermarkerFiles", INPUT_PDF + "-vannmerket.pdf");
     }
 
-    @Test(expected =  IllegalArgumentException.class)
+    private String extractText(byte[] watermarkedBytes) {
+        ByteArrayInputStream input = new ByteArrayInputStream(watermarkedBytes);
+        PDDocument pdfDocument = null;
+        try {
+            pdfDocument = PDDocument.load(input);
+            return new PDFTextStripper().getText(pdfDocument);
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        } finally {
+            try {
+                if (pdfDocument != null) {
+                    pdfDocument.close();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e.getMessage(), e);
+            }
+        }
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
     public void kasterExceptionForUlovligFil() throws IOException {
         byte[] png = getBytesFromFile("/ImageToPdfFiles/skjema1_side1.png");
         watermarker.forIdent("123123").transform(png);
