f3b416311638f480a9a85c9be6f291c01e4ced51
Rune Flobakk <Rune.Flobakk@nav.no>
2013-06-27 10:22:23 +0000

    Laget en ApplicationSecurityConfig-klasse for å sette opp bl.a. ModigCryptoMapper på en konsistent måte, og som sikrer at både den og Application instansen har samme ICryptFactory, som default er satt til KeyInSessionSunJceCryptFactory. ModigCryptoMapper har blitt scopet til package-private, og vil dermed knekke kode som instansierer denne direkte. Sue me.
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/common/trunk@43267 6ec884bf-e345-0410-bd42-d282eb737ba0

---
+++ src/main/java/no/nav/sbl/dialogarena/common/web/security/ApplicationSecurityConfig.java
@@ -0,0 +1,42 @@
+package no.nav.sbl.dialogarena.common.web.security;
+
+import no.nav.modig.security.tilgangskontroll.policy.pep.EnforcementPoint;
+import no.nav.modig.security.tilgangskontroll.wicket.BehaviorPolicyAuthorizationStrategy;
+import org.apache.wicket.core.util.crypt.KeyInSessionSunJceCryptFactory;
+import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.settings.ISecuritySettings;
+import org.apache.wicket.util.crypt.ICryptFactory;
+
+
+public final class ApplicationSecurityConfig {
+
+    private final EnforcementPoint pep;
+
+    private ICryptFactory cryptFactory = new KeyInSessionSunJceCryptFactory();
+
+    private boolean enforceMounts = true;
+
+
+    public ApplicationSecurityConfig(EnforcementPoint pep) {
+        this.pep = pep;
+    }
+
+    public final ApplicationSecurityConfig withCryptFactory(ICryptFactory cryptFactory) {
+        this.cryptFactory = cryptFactory;
+        return this;
+    }
+
+    public final ApplicationSecurityConfig doNotEnforcedMounts() {
+        this.enforceMounts = false;
+        return this;
+    }
+
+
+    public final void configure(WebApplication application) {
+        ISecuritySettings securitySettings = application.getSecuritySettings();
+        securitySettings.setEnforceMounts(enforceMounts);
+        securitySettings.setCryptFactory(cryptFactory);
+        application.setRootRequestMapper(new ModigCryptoMapper(application.getRootRequestMapper(), cryptFactory));
+        securitySettings.setAuthorizationStrategy(new BehaviorPolicyAuthorizationStrategy(pep));
+    }
+}
