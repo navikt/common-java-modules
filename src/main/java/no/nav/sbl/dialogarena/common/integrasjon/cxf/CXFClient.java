ad1e0766423ab6d175f5afb2a3900e799384512e
Hans Olaf Borch <hans.olaf.borch@nav.no>
2013-12-02 13:18:10 +0000

    common-cxf: for all your CXF needs

---
+++ src/main/java/no/nav/sbl/dialogarena/common/integrasjon/cxf/CXFClient.java
@@ -0,0 +1,89 @@
+package no.nav.sbl.dialogarena.common.integrasjon.cxf;
+
+import java.util.HashMap;
+
+import no.nav.modig.security.sts.utility.STSConfigurationUtility;
+import no.nav.sbl.dialogarena.common.integrasjon.features.TimeoutFeature;
+
+import org.apache.cxf.configuration.jsse.TLSClientParameters;
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.feature.LoggingFeature;
+import org.apache.cxf.frontend.ClientProxy;
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.apache.cxf.transport.http.HTTPConduit;
+import org.apache.cxf.ws.addressing.WSAddressingFeature;
+
+public class CXFClient<T> {
+
+	public final JaxWsProxyFactoryBean factoryBean = new JaxWsProxyFactoryBean();
+	private final Class<T> serviceClass;
+	private boolean configureStsForExternalSSO, configureStsForSystemUser;
+	private int connectionTimeout = TimeoutFeature.DEFAULT_CONNECTION_TIMEOUT;
+	private int receiveTimeout = TimeoutFeature.DEFAULT_RECEIVE_TIMEOUT;
+
+	public CXFClient(Class<T> serviceClass) {
+		factoryBean.getFeatures().add(new LoggingFeature());
+		factoryBean.getFeatures().add(new WSAddressingFeature());
+		factoryBean.setProperties(new HashMap<String, Object>());
+		this.serviceClass = serviceClass;
+	}
+
+	public CXFClient<T> address(String url) {
+		factoryBean.setAddress(url);
+		return this;
+	}
+
+	public CXFClient<T> wsdl(String url) {
+		factoryBean.setWsdlURL(url);
+		return this;
+	}
+
+	public CXFClient<T> configureStsForExternalSSO() {
+		configureStsForExternalSSO = true;
+		return this;
+	}
+
+	public CXFClient<T> configureStsForSystemUser() {
+		configureStsForSystemUser = true;
+		return this;
+	}
+
+	public CXFClient<T> setProperty(String key, Object value) {
+		factoryBean.getProperties().put(key, value);
+		return this;
+	}
+
+	public CXFClient<T> timeout(int connectionTimeout, int receiveTimeout) {
+		this.connectionTimeout = connectionTimeout;
+		this.receiveTimeout = receiveTimeout;
+		return this;
+	}
+
+	public CXFClient<T> enableMtom() {
+		factoryBean.getProperties().put("mtom-enabled", true);
+		return this;
+	}
+
+	public T build() {
+		factoryBean.getFeatures().add(new TimeoutFeature(receiveTimeout, connectionTimeout));
+		T portType = factoryBean.create(serviceClass);
+		Client client = ClientProxy.getClient(portType);
+        disableCNCheckIfConfigured(client);
+        if (configureStsForExternalSSO) {
+        	STSConfigurationUtility.configureStsForExternalSSO(client);
+        }
+        if (configureStsForSystemUser) {
+        	STSConfigurationUtility.configureStsForSystemUser(client);
+        }
+        return portType;
+	}
+
+    private static void disableCNCheckIfConfigured(Client client) {
+        HTTPConduit httpConduit = (HTTPConduit) client.getConduit();
+        httpConduit.setTlsClientParameters(new TLSClientParameters());
+        if (Boolean.valueOf(System.getProperty("disable.ssl.cn.check", "false"))) {
+            httpConduit.getTlsClientParameters().setDisableCNCheck(true);
+        }
+    }
+
+}
---
+++ src/main/java/no/nav/sbl/dialogarena/common/integrasjon/cxf/CXFEndpoint.java
@@ -0,0 +1,54 @@
+package no.nav.sbl.dialogarena.common.integrasjon.cxf;
+
+import static java.util.Arrays.asList;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import no.nav.modig.security.ws.SAMLInInterceptor;
+import no.nav.sbl.dialogarena.common.integrasjon.features.AttachmentCleanupInterceptor;
+
+import org.apache.cxf.feature.LoggingFeature;
+import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
+import org.apache.cxf.ws.addressing.WSAddressingFeature;
+
+public class CXFEndpoint {
+
+	public final JaxWsServerFactoryBean factoryBean;
+
+	public CXFEndpoint() {
+		factoryBean = new JaxWsServerFactoryBean();
+		Map<String, Object> properties = new HashMap<>();
+		properties.put("schema-validation-enabled", true);
+		factoryBean.setProperties(properties);
+		factoryBean.getInInterceptors().add(new SAMLInInterceptor());
+		factoryBean.setFeatures(asList(new LoggingFeature(), new WSAddressingFeature()));
+	}
+
+	public CXFEndpoint enableMtom() {
+		factoryBean.getProperties().put("mtom-enabled", true);
+		factoryBean.getOutInterceptors().add(new AttachmentCleanupInterceptor());
+		factoryBean.getOutFaultInterceptors().add(new AttachmentCleanupInterceptor());
+		return this;
+	}
+
+	public CXFEndpoint address(String address) {
+		factoryBean.setAddress(address);
+		return this;
+	}
+
+	public CXFEndpoint serviceBean(Object serviceBean) {
+		factoryBean.setServiceBean(serviceBean);
+		return this;
+	}
+
+	public CXFEndpoint setProperty(String key, Object value) {
+		factoryBean.getProperties().put(key, value);
+		return this;
+	}
+
+	public void create() {
+		factoryBean.create();
+	}
+
+}
