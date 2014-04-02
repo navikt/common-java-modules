027fff27e3994150b0e71533056849a5d8a6f8e0
Borge Bardo <borge.bardo@nav.no>
2014-04-02 13:27:22 +0200

    la til metoder for å sette opp handlere og outinterceptors på klienten

--- src/main/java/no/nav/sbl/dialogarena/common/cxf/CXFClient.java
+++ src/main/java/no/nav/sbl/dialogarena/common/cxf/CXFClient.java
@@ -1,81 +1,101 @@
 package no.nav.sbl.dialogarena.common.cxf;
 
-import java.util.HashMap;
-
 import no.nav.modig.security.sts.utility.STSConfigurationUtility;
-
 import org.apache.cxf.configuration.jsse.TLSClientParameters;
 import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.feature.LoggingFeature;
 import org.apache.cxf.frontend.ClientProxy;
+import org.apache.cxf.interceptor.Interceptor;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.apache.cxf.message.Message;
 import org.apache.cxf.transport.http.HTTPConduit;
 import org.apache.cxf.ws.addressing.WSAddressingFeature;
 
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.handler.Handler;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 public class CXFClient<T> {
 
-	public final JaxWsProxyFactoryBean factoryBean = new JaxWsProxyFactoryBean();
-	private final Class<T> serviceClass;
-	private boolean configureStsForExternalSSO, configureStsForSystemUser;
-	private int connectionTimeout = TimeoutFeature.DEFAULT_CONNECTION_TIMEOUT;
-	private int receiveTimeout = TimeoutFeature.DEFAULT_RECEIVE_TIMEOUT;
+    public final JaxWsProxyFactoryBean factoryBean = new JaxWsProxyFactoryBean();
+    private final Class<T> serviceClass;
+    private final List<Handler> handlerChain = new ArrayList<>();
+    private boolean configureStsForExternalSSO, configureStsForSystemUser;
+    private int connectionTimeout = TimeoutFeature.DEFAULT_CONNECTION_TIMEOUT;
+    private int receiveTimeout = TimeoutFeature.DEFAULT_RECEIVE_TIMEOUT;
 
-	public CXFClient(Class<T> serviceClass) {
-		factoryBean.getFeatures().add(new LoggingFeature());
-		factoryBean.getFeatures().add(new WSAddressingFeature());
-		factoryBean.setProperties(new HashMap<String, Object>());
-		this.serviceClass = serviceClass;
-	}
+    public CXFClient(Class<T> serviceClass) {
+        factoryBean.getFeatures().add(new LoggingFeature());
+        factoryBean.getFeatures().add(new WSAddressingFeature());
+        factoryBean.setProperties(new HashMap<String, Object>());
+        this.serviceClass = serviceClass;
+    }
 
-	public CXFClient<T> address(String url) {
-		factoryBean.setAddress(url);
-		return this;
-	}
+    public CXFClient<T> address(String url) {
+        factoryBean.setAddress(url);
+        return this;
+    }
 
-	public CXFClient<T> wsdl(String url) {
-		factoryBean.setWsdlURL(url);
-		return this;
-	}
+    public CXFClient<T> wsdl(String url) {
+        factoryBean.setWsdlURL(url);
+        return this;
+    }
 
-	public CXFClient<T> configureStsForExternalSSO() {
-		configureStsForExternalSSO = true;
-		return this;
-	}
+    public CXFClient<T> configureStsForExternalSSO() {
+        configureStsForExternalSSO = true;
+        return this;
+    }
 
-	public CXFClient<T> configureStsForSystemUser() {
-		configureStsForSystemUser = true;
-		return this;
-	}
+    public CXFClient<T> configureStsForSystemUser() {
+        configureStsForSystemUser = true;
+        return this;
+    }
 
-	public CXFClient<T> setProperty(String key, Object value) {
-		factoryBean.getProperties().put(key, value);
-		return this;
-	}
+    public CXFClient<T> setProperty(String key, Object value) {
+        factoryBean.getProperties().put(key, value);
+        return this;
+    }
 
-	public CXFClient<T> timeout(int connectionTimeout, int receiveTimeout) {
-		this.connectionTimeout = connectionTimeout;
-		this.receiveTimeout = receiveTimeout;
-		return this;
-	}
+    public CXFClient<T> timeout(int connectionTimeout, int receiveTimeout) {
+        this.connectionTimeout = connectionTimeout;
+        this.receiveTimeout = receiveTimeout;
+        return this;
+    }
 
-	public CXFClient<T> enableMtom() {
-		factoryBean.getProperties().put("mtom-enabled", true);
-		return this;
-	}
+    public CXFClient<T> enableMtom() {
+        factoryBean.getProperties().put("mtom-enabled", true);
+        return this;
+    }
 
-	public T build() {
-		factoryBean.getFeatures().add(new TimeoutFeature(receiveTimeout, connectionTimeout));
-		T portType = factoryBean.create(serviceClass);
-		Client client = ClientProxy.getClient(portType);
+    public CXFClient<T> withHandler(Handler... handlers) {
+        handlerChain.addAll(asList(handlers));
+        return this;
+    }
+
+    @SafeVarargs
+    public final CXFClient<T> withOutInterceptor(Interceptor<? extends Message>... interceptors) {
+        factoryBean.getOutInterceptors().addAll(asList(interceptors));
+        return this;
+    }
+
+    public T build() {
+        factoryBean.getFeatures().add(new TimeoutFeature(receiveTimeout, connectionTimeout));
+        T portType = factoryBean.create(serviceClass);
+        Client client = ClientProxy.getClient(portType);
         disableCNCheckIfConfigured(client);
         if (configureStsForExternalSSO) {
-        	STSConfigurationUtility.configureStsForExternalSSO(client);
+            STSConfigurationUtility.configureStsForExternalSSO(client);
         }
         if (configureStsForSystemUser) {
-        	STSConfigurationUtility.configureStsForSystemUser(client);
+            STSConfigurationUtility.configureStsForSystemUser(client);
         }
+        ((BindingProvider) portType).getBinding().setHandlerChain(handlerChain);
         return portType;
-	}
+    }
 
     private static void disableCNCheckIfConfigured(Client client) {
         HTTPConduit httpConduit = (HTTPConduit) client.getConduit();
