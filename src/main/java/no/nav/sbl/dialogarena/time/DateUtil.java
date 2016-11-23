dce89bf12729c3cf1412d21de124e7be480cfe5a
Andreas Lund <andreas.lund@nav.no>
2016-11-23 14:21:31 +0100

    fraKortDatoMedTid

--- src/main/java/no/nav/sbl/dialogarena/time/DateUtil.java
+++ src/main/java/no/nav/sbl/dialogarena/time/DateUtil.java
@@ -3,6 +3,7 @@ package no.nav.sbl.dialogarena.time;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import static java.time.LocalDateTime.*;
 import static java.time.format.DateTimeFormatter.ofPattern;
 
 public class DateUtil {
@@ -16,5 +17,9 @@ public class DateUtil {
 
     public static String tilKortDatoMedTid(LocalDateTime dato){
         return dato.format(ofPattern("dd.MM.yyyy HH:mm"));
+    }
+
+    public static LocalDateTime fraKortDatoMedTid(String dato){
+        return parse(dato, ofPattern("dd.MM.yyyy HH:mm"));
     }
 }
