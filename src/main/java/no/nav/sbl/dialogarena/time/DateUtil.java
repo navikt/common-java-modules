efc224ad08d2e2dd994ee879a5d4bc091268e07d
Andreas Lund <andreas.lund@nav.no>
2017-01-23 09:07:35 +0100

    mulighet til å ha lang dato med klokke etter og dag før.

--- src/main/java/no/nav/sbl/dialogarena/time/DateUtil.java
+++ src/main/java/no/nav/sbl/dialogarena/time/DateUtil.java
@@ -1,7 +1,10 @@
 package no.nav.sbl.dialogarena.time;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.TextStyle;
+import java.util.Locale;
 
 import static java.time.LocalDateTime.*;
 import static java.time.format.DateTimeFormatter.ofPattern;
@@ -30,5 +33,29 @@ public class DateUtil {
 
     public static String tilLangDatoMedKlokkeslettPostfix(LocalDateTime dato) {
         return dato.format(ofPattern("dd.MM.yyyy")) + " kl. " + dato.format(ofPattern("HH:mm"));
+    }
+
+    public static String tilLangDatoMedKlokkeslettPostfixDagPrefix(LocalDateTime dato) {
+        return dag(dato.getDayOfWeek()) + " " + dato.format(ofPattern("dd.MM.yyyy")) + " kl. " + dato.format(ofPattern("HH:mm"));
+    }
+
+    private static String dag(DayOfWeek dayOfWeek) {
+        switch (dayOfWeek) {
+            case MONDAY:
+                return "Mandag";
+            case TUESDAY:
+                return "Tirsdag";
+            case WEDNESDAY:
+                return "Onsdag";
+            case THURSDAY:
+                return "Torsdag";
+            case FRIDAY:
+                return "Fredag";
+            case SATURDAY:
+                return "Lørdag";
+            case SUNDAY:
+                return "Søndag";
+        }
+        return "";
     }
 }
