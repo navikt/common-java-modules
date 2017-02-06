6616dfa6fc7d93bdf142b2b1bad9bcfe53584bd2
Enger <E148211@ADEO.NO>
2017-02-06 16:20:29 +0100

    Egen metode for felles mockdata i requester

--- src/test/java/no/nav/sbl/dialogarena/common/abac/pep/MockXacmlRequest.java
+++ src/test/java/no/nav/sbl/dialogarena/common/abac/pep/MockXacmlRequest.java
@@ -14,29 +14,29 @@ public class MockXacmlRequest {
         return new XacmlRequest().withRequest(getRequestWithSubjAttrWithoutEnvironment());
     }
 
-    private static Request getRequest() {
+    private static Request getRequestWithActionAndResource() {
         final Action action = new Action();
         action.getAttribute().add(new Attribute("urn:oasis:names:tc:xacml:1.0:action:action-id", "read"));
 
+        final Resource resource = new Resource();
+        resource.getAttribute().add(new Attribute("no.nav.abac.attributter.resource.felles.resource_type", "no.nav.abac.attributter.resource.felles.person"));
+        resource.getAttribute().add(new Attribute("no.nav.abac.attributter.resource.felles.domene", "Foreldrepenger"));
+        resource.getAttribute().add(new Attribute("no.nav.abac.attributter.resource.felles.person.fnr", "01010122222"));
+
+        return new Request()
+                .withAction(action)
+                .withResource(resource);
+    }
+
+    private static Request getRequest() {
         final Environment environment = new Environment();
         environment.getAttribute().add(new Attribute("no.nav.abac.attributter.environment.felles.oidc_token_body", "eyJpc3MiOiJuYXYubm8iLCJleHAiOjE0ODQ2NTI2NzIsImp0aSI6IkZHdXJVYWdleFRwTUVZTjdMRHlsQ1EiLCJpYXQiOjE0ODQ2NTIwNzIsIm5iZiI6MTQ4NDY1MTk1Miwic3ViIjoiYTExMTExMSJ9"));
         environment.getAttribute().add(new Attribute("no.nav.abac.attributter.environment.felles.pep_id", "srvEksempelPep"));
 
-        final Resource resource = new Resource();
-        resource.getAttribute().add(new Attribute("no.nav.abac.attributter.resource.felles.resource_type", "no.nav.abac.attributter.resource.felles.person"));
-        resource.getAttribute().add(new Attribute("no.nav.abac.attributter.resource.felles.domene", "Foreldrepenger"));
-        resource.getAttribute().add(new Attribute("no.nav.abac.attributter.resource.felles.person.fnr", "01010122222"));
-
-        return new Request()
-                .withAction(action)
-                .withEnvironment(environment)
-                .withResource(resource);
+        return getRequestWithActionAndResource().withEnvironment(environment);
     }
 
     private static Request getRequestWithSubjectAttributes() {
-        final Action action = new Action();
-        action.getAttribute().add(new Attribute("urn:oasis:names:tc:xacml:1.0:action:action-id", "read"));
-
         final Environment environment = new Environment();
         environment.getAttribute().add(new Attribute("no.nav.abac.attributter.environment.felles.pep_id", "srvEksempelPep"));
 
@@ -44,34 +44,16 @@ public class MockXacmlRequest {
         accessSubject.getAttribute().add(new Attribute("urn:oasis:names:tc:xacml:1.0:subject:subject-id", "A111111"));
         accessSubject.getAttribute().add(new Attribute("no.nav.abac.attributter.subject.felles.subjectType", "InternBruker"));
 
-        final Resource resource = new Resource();
-        resource.getAttribute().add(new Attribute("no.nav.abac.attributter.resource.felles.resource_type", "no.nav.abac.attributter.resource.felles.person"));
-        resource.getAttribute().add(new Attribute("no.nav.abac.attributter.resource.felles.domene", "Foreldrepenger"));
-        resource.getAttribute().add(new Attribute("no.nav.abac.attributter.resource.felles.person.fnr", "01010122222"));
-
-        return new Request()
+        return getRequestWithActionAndResource()
                 .withAccessSubject(accessSubject)
-                .withEnvironment(environment)
-                .withAction(action)
-                .withResource(resource);
+                .withEnvironment(environment);
     }
 
     private static Request getRequestWithSubjAttrWithoutEnvironment() {
-        final Action action = new Action();
-        action.getAttribute().add(new Attribute("urn:oasis:names:tc:xacml:1.0:action:action-id", "read"));
-
         final AccessSubject accessSubject = new AccessSubject();
         accessSubject.getAttribute().add(new Attribute("urn:oasis:names:tc:xacml:1.0:subject:subject-id", "A111111"));
         accessSubject.getAttribute().add(new Attribute("no.nav.abac.attributter.subject.felles.subjectType", "InternBruker"));
 
-        final Resource resource = new Resource();
-        resource.getAttribute().add(new Attribute("no.nav.abac.attributter.resource.felles.resource_type", "no.nav.abac.attributter.resource.felles.person"));
-        resource.getAttribute().add(new Attribute("no.nav.abac.attributter.resource.felles.domene", "Foreldrepenger"));
-        resource.getAttribute().add(new Attribute("no.nav.abac.attributter.resource.felles.person.fnr", "01010122222"));
-
-        return new Request()
-                .withAccessSubject(accessSubject)
-                .withAction(action)
-                .withResource(resource);
+        return getRequestWithActionAndResource().withAccessSubject(accessSubject);
     }
 }
