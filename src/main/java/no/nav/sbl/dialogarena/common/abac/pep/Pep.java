435b0f19dc8280b41be9e04e6ebe7fdf997707d7
Enger <E148211@ADEO.NO>
2017-02-06 09:43:44 +0100

    metode som bygger opp et Request-objekt

--- src/main/java/no/nav/sbl/dialogarena/common/abac/pep/Pep.java
+++ src/main/java/no/nav/sbl/dialogarena/common/abac/pep/Pep.java
@@ -25,6 +25,29 @@ public class Pep {
         this.pdpService = pdpService;
     }
 
+    private Request buildRequest(String oidcToken, String subjectId, String domain, String fnr, String credentialResource) {
+        if (domain == null || fnr == null || credentialResource == null || (oidcToken == null && subjectId == null)) { return null; }
+
+        final Environment environment = new Environment();
+        if (oidcToken != null) {
+            environment.getAttribute().add(new Attribute("no.nav.abac.attributter.environment.felles.oidc_token_body", oidcToken));
+        }
+        environment.getAttribute().add(new Attribute("no.nav.abac.attributter.environment.felles.pep_id", credentialResource));
+
+        final Action action = new Action();
+        action.getAttribute().add(new Attribute("urn:oasis:names:tc:xacml:1.0:action:action-id", "read"));
+
+        final Resource resource = new Resource();
+        resource.getAttribute().add(new Attribute("no.nav.abac.attributter.resource.felles.resource_type", "no.nav.abac.attributter.resource.felles.person"));
+        resource.getAttribute().add(new Attribute("no.nav.abac.attributter.resource.felles.domene", domain));
+        resource.getAttribute().add(new Attribute("no.nav.abac.attributter.resource.felles.person.fnr", fnr));
+
+        return new Request()
+                .withEnvironment(environment)
+                .withAction(action)
+                .withResource(resource);
+    }
+
     BiasedDecisionResponse evaluateWithBias(XacmlRequest request) {
         log.debug("evaluating request with bias:" + bias);
         XacmlResponse response = pdpService.askForPermission(request);
