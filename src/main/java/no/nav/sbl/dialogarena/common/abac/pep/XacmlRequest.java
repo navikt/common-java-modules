544c33a0a8c1fd4c850103bbce01259831bf396b
Joanna Eriksson <joanna.eriksson@nav.no>
2017-02-08 10:35:51 +0100

    PK-38701, PKULV-2136: Fjernet SerializedName da den ikke trengs lengre

--- src/main/java/no/nav/sbl/dialogarena/common/abac/pep/XacmlRequest.java
+++ src/main/java/no/nav/sbl/dialogarena/common/abac/pep/XacmlRequest.java
@@ -1,11 +1,8 @@
 package no.nav.sbl.dialogarena.common.abac.pep;
 
 
-import com.google.gson.annotations.SerializedName;
-
 public class XacmlRequest {
 
-    @SerializedName("Request")
     private Request request;
 
     XacmlRequest withRequest(Request request) {
