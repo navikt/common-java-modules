1ad4af1b95a0102348b658aa5b4a3ad919d2f633
Joanna Eriksson <joanna.eriksson@nav.no>
2017-02-09 12:38:44 +0100

    PK-38701, PKULV-2136: Lagt til test av PdpService som manglade

--- src/test/java/no/nav/sbl/dialogarena/common/abac/PdpServiceTest.java
+++ src/test/java/no/nav/sbl/dialogarena/common/abac/PdpServiceTest.java
@@ -4,15 +4,20 @@ import mockit.Expectations;
 import mockit.integration.junit4.JMockit;
 import no.nav.sbl.dialogarena.common.abac.pep.MockXacmlRequest;
 import no.nav.sbl.dialogarena.common.abac.pep.PdpService;
-import no.nav.sbl.dialogarena.common.abac.pep.domain.response.XacmlResponse;
+import no.nav.sbl.dialogarena.common.abac.pep.domain.response.*;
 import org.apache.http.client.methods.HttpPost;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static no.nav.sbl.dialogarena.common.abac.TestUtils.getContentFromJsonFile;
 import static no.nav.sbl.dialogarena.common.abac.TestUtils.prepareResponse;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 @RunWith(JMockit.class)
 public class PdpServiceTest {
@@ -28,8 +33,28 @@ public class PdpServiceTest {
 
         final XacmlResponse actualXacmlResponse = pdpService.askForPermission(MockXacmlRequest.getXacmlRequest());
 
-        //TODO Assert....
 
+        final XacmlResponse expectedXacmlResponse = getExpectedXacmlResponse();
+
+        assertThat(actualXacmlResponse, is(equalTo(expectedXacmlResponse)));
+
+    }
+
+    private XacmlResponse getExpectedXacmlResponse() {
+        final Advice advice = new Advice("no.nav.abac.advices.deny.reason",
+                new AttributeAssignment("no.nav.abac.advice.fritekst", "Ikke tilgang"));
+
+        final List<Advice> associatedAdvice = new ArrayList<>();
+        associatedAdvice.add(advice);
+
+        final Response response = new Response()
+                .withDecision(Decision.Deny)
+                .withAssociatedAdvice(associatedAdvice);
+        final List<Response> responses = new ArrayList<>();
+        responses.add(response);
+
+        return new XacmlResponse()
+                .withResponse(responses);
     }
 
 
