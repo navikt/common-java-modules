8c785c209754b0b353d3d1b17bf044dc28214658
Enger <E148211@ADEO.NO>
2017-02-21 14:19:18 +0100

    PK-38701 PKULV-2241: La til toString-metoder i Advice og Attribute slik at logging av advices blir penere

--- src/main/java/no/nav/sbl/dialogarena/common/abac/pep/domain/Attribute.java
+++ src/main/java/no/nav/sbl/dialogarena/common/abac/pep/domain/Attribute.java
@@ -13,4 +13,8 @@ public class Attribute {
         this.value = value;
     }
 
+    @Override
+    public String toString() {
+        return "AttributeId: " + attributeId + "Attribute value: " + value;
+    }
 }
