eafa0fd9e8e32a4adbfe350b440b271d2cfa67b6
Joanna Eriksson <joanna.eriksson@nav.no>
2017-03-13 13:52:27 +0100

    PK-38701 PKULV-2141 PKULV-2145: Add a new constructor to use original exception

--- src/main/java/no/nav/sbl/dialogarena/common/abac/pep/exception/AbacException.java
+++ src/main/java/no/nav/sbl/dialogarena/common/abac/pep/exception/AbacException.java
@@ -1,7 +1,13 @@
 package no.nav.sbl.dialogarena.common.abac.pep.exception;
 
+import java.io.IOException;
+
 public class AbacException extends Exception {
     public AbacException(String message) {
         super(message);
+    }
+
+    public AbacException(String s, IOException e) {
+        super(s, e);
     }
 }
