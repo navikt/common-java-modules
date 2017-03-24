da7fb7c3ba47f6d7fa8061f5b3a9d9c107a27c4d
Joanna Eriksson <joanna.eriksson@nav.no>
2017-03-24 08:59:50 +0100

    PK-38701, PKULV-2141 PKULV-2145: add documentation of interface

--- src/main/java/no/nav/sbl/dialogarena/common/abac/pep/Pep.java
+++ src/main/java/no/nav/sbl/dialogarena/common/abac/pep/Pep.java
@@ -4,8 +4,25 @@ import no.nav.sbl.dialogarena.common.abac.pep.domain.response.BiasedDecisionResp
 import no.nav.sbl.dialogarena.common.abac.pep.exception.PepException;
 
 public interface Pep {
+
+    /**
+     *
+     * @param oidcToken the body of an oidc token
+     * @param domain domain for applikasjon, f eks veilarb
+     * @param fnr fnr of the person
+     * @return decision from ABAC together with the full response from ABAC
+     * @throws PepException a general exception that indicated that something went wrong in the library
+     */
     BiasedDecisionResponse isServiceCallAllowedWithOidcToken(String oidcToken, String domain, String fnr) throws PepException;
 
+    /**
+     *
+     * @param subjectId ident og saksbehandler
+     * @param domain domain for applikasjon, f eks veilarb
+     * @param fnr fnr of the person
+     * @return decision from ABAC together with the full response from ABAC
+     * @throws PepException a general exception that indicated that something went wrong in the library
+     */
     BiasedDecisionResponse isServiceCallAllowedWithIdent(String subjectId, String domain, String fnr) throws PepException;
 
 }
