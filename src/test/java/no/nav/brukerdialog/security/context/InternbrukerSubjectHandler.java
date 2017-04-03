8d69ef488a7600d1a8ee2d80cb744987aced07a9
Andreas Lund <andreas.lund@nav.no>
2017-04-03 13:18:00 +0200

    Mulighet for å sette veilederIdent og serviceBruker eksplisitt.

--- src/test/java/no/nav/brukerdialog/security/context/InternbrukerSubjectHandler.java
+++ src/test/java/no/nav/brukerdialog/security/context/InternbrukerSubjectHandler.java
@@ -6,26 +6,25 @@ import no.nav.brukerdialog.security.domain.SluttBruker;
 
 import javax.security.auth.Subject;
 
+import static java.lang.System.getProperty;
+import static java.lang.System.setProperty;
+
 public class InternbrukerSubjectHandler extends TestSubjectHandler {
-    private static final Subject DEFAULT_SUBJECT;
-    private static String veilederIdent;
-    private static String serviceBruker;
+    private Subject subject;
 
-    public InternbrukerSubjectHandler(String vIdent, String srvBruker) {
-        veilederIdent = vIdent;
-        serviceBruker = srvBruker;
+    public static void setVeilederIdent(String veilederident) {
+        setProperty("no.nav.brukerdialog.security.context.InternbrukerSubjectHandler.veilederident", veilederident);
     }
 
-    static {
-        DEFAULT_SUBJECT = new Subject();
-        DEFAULT_SUBJECT.getPrincipals().add(new SluttBruker(veilederIdent, IdentType.InternBruker));
-        DEFAULT_SUBJECT.getPrincipals().add(new ConsumerId(serviceBruker));
+    public static void setServicebruker(String servicebruker) {
+        setProperty("no.nav.brukerdialog.security.context.InternbrukerSubjectHandler.servicebruker", servicebruker);
     }
-
-    private static Subject subject = DEFAULT_SUBJECT;
 
     @Override
     public Subject getSubject() {
+        Subject subject = new Subject();
+        subject.getPrincipals().add(new SluttBruker(getProperty("no.nav.brukerdialog.security.context.InternbrukerSubjectHandler.veilederident", "Z999999"), IdentType.InternBruker));
+        subject.getPrincipals().add(new ConsumerId(getProperty("no.nav.brukerdialog.security.context.InternbrukerSubjectHandler.servicebruker", "srvServiceBruker")));
         return subject;
     }
 
@@ -36,6 +35,6 @@ public class InternbrukerSubjectHandler extends TestSubjectHandler {
 
     @Override
     public void reset() {
-        setSubject(DEFAULT_SUBJECT);
+        setSubject(subject);
     }
 }
