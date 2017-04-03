f4bda5c0a07417772fef1ceb404cd225a3f7e58d
Andreas Lund <andreas.lund@nav.no>
2017-04-03 09:04:31 +0200

    Litt mer bakoverkompabilitet.

--- src/main/java/no/nav/brukerdialog/security/jaspic/AuthorizationRequestBuilder.java
+++ src/main/java/no/nav/brukerdialog/security/jaspic/AuthorizationRequestBuilder.java
@@ -1,23 +1,27 @@
 package no.nav.brukerdialog.security.jaspic;
 
-
 import no.nav.brukerdialog.tools.HostUtils;
-
 import javax.servlet.http.HttpServletRequest;
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.net.URLEncoder;
 import java.security.SecureRandom;
 
+import static java.lang.System.*;
+
 public class AuthorizationRequestBuilder {
 
     private static final SecureRandom random = new SecureRandom();
 
+    private final String server;
     private final String scope = "openid";
     private boolean useKerberos = true;
     private String stateIndex;
+    private static final String DEFAULT_REDIRECT_URL = "/" + getProperty("applicationName") + "/tjenester/login";
 
-    public AuthorizationRequestBuilder() {
+
+    public AuthorizationRequestBuilder(HttpServletRequest req) {
+        server = HostUtils.formatSchemeHostPort(req);
         byte bytes[] = new byte[20];
         random.nextBytes(bytes);
         stateIndex = "state_" + new BigInteger(1, bytes).toString(16);
@@ -33,19 +37,18 @@ public class AuthorizationRequestBuilder {
     }
 
     public String buildRedirectString() throws UnsupportedEncodingException {
-        String clientId = System.getProperty("isso-rp-user.username");
+        String clientId = getProperty("isso-rp-user.username");
         String state = stateIndex;
+        String redirectUrl = getProperty("oidc-redirect.url") == null ? server + DEFAULT_REDIRECT_URL : getProperty("oidc-redirect.url");
         String kerberosTrigger = useKerberos
                 ? "session=winssochain&authIndexType=service&authIndexValue=winssochain&"
                 : "";
         return String.format("%s/authorize?" + kerberosTrigger + "response_type=code&scope=%s&client_id=%s&state=%s&redirect_uri=%s",
-                System.getProperty("isso-host.url"),
+                getProperty("isso-host.url"),
                 scope,
                 URLEncoder.encode(clientId, "UTF-8"),
                 state,
-                URLEncoder.encode(System.getProperty("oidc-redirect.url"), "UTF-8")
+                URLEncoder.encode(redirectUrl, "UTF-8")
         );
     }
-
-
 }
--- src/main/java/no/nav/brukerdialog/security/jaspic/OidcAuthModule.java
+++ src/main/java/no/nav/brukerdialog/security/jaspic/OidcAuthModule.java
@@ -45,7 +45,7 @@ public class OidcAuthModule implements ServerAuthModule {
 
     private static final boolean sslOnlyCookies = !Boolean.valueOf(System.getProperty("develop-local", "false"));
     public static final String REFRESH_TIME = "no.nav.kes.security.oidc.minimum_time_to_expire_before_refresh.seconds";
-    public static final String OIDCLOGIN_REDIRECT_URL = "/oidclogin/login";
+    private static final String DEFAULT_REDIRECT_URL = "/"+System.getProperty("applicationName")+"/tjenester/login";
 
     private final OidcTokenValidator tokenValidator;
     private final IdTokenProvider tokenProvider;
@@ -159,10 +159,12 @@ public class OidcAuthModule implements ServerAuthModule {
     }
 
     private void addApplicationCallbackSpecificHttpOnlyCookie(HttpServletResponse response, String name, String value) {
+        String redirectUrl = System.getProperty("oidc-redirect.url") == null ? DEFAULT_REDIRECT_URL : "/oidclogin/login";
+
         Cookie cookie = new Cookie(name, value);
         cookie.setSecure(sslOnlyCookies);
         cookie.setHttpOnly(true);
-        cookie.setPath(OIDCLOGIN_REDIRECT_URL);
+        cookie.setPath(redirectUrl);
         response.addCookie(cookie);
     }
 
@@ -173,7 +175,7 @@ public class OidcAuthModule implements ServerAuthModule {
             if ("application/json".equals(request.getHeader("Accept"))) {
                 response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Resource is protected, but id token is missing or invalid.");
             } else {
-                AuthorizationRequestBuilder builder = new AuthorizationRequestBuilder();
+                AuthorizationRequestBuilder builder = new AuthorizationRequestBuilder(request);
                 //TODO CSRF attack protection. See RFC-6749 section 10.12 (the state-cookie containing redirectURL shold be encrypted to avoid tampering)
                 addApplicationCallbackSpecificHttpOnlyCookie(response, builder.getStateIndex(), encode(getOriginalUrl(request)));
                 response.sendRedirect(builder.buildRedirectString());
