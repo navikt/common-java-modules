96fb8c308e6409c08a2b111b38a4f6c05a639e93
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2017-05-11 13:45:44 +0200

    gjorde det mulig å parameterisere IdTokenAndRefreshTokenProvider

--- src/main/java/no/nav/brukerdialog/security/oidc/IdTokenAndRefreshTokenProvider.java
+++ src/main/java/no/nav/brukerdialog/security/oidc/IdTokenAndRefreshTokenProvider.java
@@ -1,5 +1,6 @@
 package no.nav.brukerdialog.security.oidc;
 
+import lombok.Builder;
 import no.nav.brukerdialog.security.domain.IdTokenAndRefreshToken;
 import no.nav.brukerdialog.security.domain.OidcCredential;
 import no.nav.brukerdialog.tools.HostUtils;
@@ -13,41 +14,58 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 import static java.lang.System.getProperty;
+import static javax.ws.rs.core.HttpHeaders.*;
 
 public class IdTokenAndRefreshTokenProvider {
 
     private static final Logger log = LoggerFactory.getLogger(IdTokenAndRefreshTokenProvider.class);
-    private static final String DEFAULT_REDIRECT_URL = "/"+ getProperty("applicationName")+"/tjenester/login";
+
+    public static final String ISSO_HOST_URL_PROPERTY_NAME = "isso-host.url";
+    public static final String ISSO_RP_USER_USERNAME_PROPERTY_NAME = "isso-rp-user.username";
+    public static final String ISSO_RP_USER_PASSWORD_PROPERTY_NAME = "isso-rp-user.password";
 
     static final String ENCODING = "UTF-8";
+
+    private static final String DEFAULT_REDIRECT_URL = "/"+ getProperty("applicationName")+"/tjenester/login";
+    private final Parameters parameters;
+
+    public IdTokenAndRefreshTokenProvider() {
+        this(Parameters.builder()
+                // TODO er dette korrekt? eller skal det være: getProperty("oidc-redirect.url") == null ? getProperty("oidc-redirect.url") : "/oidclogin/login";
+                .redirectUrl(getProperty("oidc-redirect.url") == null ? DEFAULT_REDIRECT_URL : "/oidclogin/login")
+                //
+                .host(getProperty(ISSO_HOST_URL_PROPERTY_NAME))
+                .username(getProperty(ISSO_RP_USER_USERNAME_PROPERTY_NAME))
+                .password(getProperty(ISSO_RP_USER_PASSWORD_PROPERTY_NAME))
+                .build()
+        );
+    }
+
+    public IdTokenAndRefreshTokenProvider(Parameters parameters) {
+        this.parameters = parameters;
+    }
 
     public IdTokenAndRefreshToken getToken(String authorizationCode, UriInfo uri) {
         return TokenProviderUtil.getToken(() -> createTokenRequest(authorizationCode, uri), s -> extractToken(s));
     }
 
     HttpPost createTokenRequest(String authorizationCode, UriInfo redirectUri) {
-        String redirectUrl = getProperty("oidc-redirect.url") == null ? DEFAULT_REDIRECT_URL : "/oidclogin/login";
         String urlEncodedRedirectUri;
         try {
-            urlEncodedRedirectUri = URLEncoder.encode(HostUtils.formatSchemeHostPort(redirectUri) + redirectUrl, ENCODING);
+            urlEncodedRedirectUri = URLEncoder.encode(HostUtils.formatSchemeHostPort(redirectUri) + parameters.redirectUrl, ENCODING);
         } catch (UnsupportedEncodingException e) {
             throw new IllegalArgumentException("Could not URL-encode the redirectUri: " + redirectUri);
         }
 
-        String host = getProperty("isso-host.url");
-        String realm = "/";
-        String username = getProperty("isso-rp-user.username");
-        String password = getProperty("isso-rp-user.password");
-
-        HttpPost request = new HttpPost(host + "/access_token");
-        request.setHeader("Authorization", TokenProviderUtil.basicCredentials(username, password));
-        request.setHeader("Cache-Control", "no-cache");
-        request.setHeader("Content-type", "application/x-www-form-urlencoded");
+        HttpPost request = new HttpPost(parameters.host + "/access_token");
+        request.setHeader(AUTHORIZATION, TokenProviderUtil.basicCredentials(parameters.username, parameters.password));
+        request.setHeader(CACHE_CONTROL, "no-cache");
+        request.setHeader(CONTENT_TYPE, "application/x-www-form-urlencoded");
         String data = "grant_type=authorization_code"
-                + "&realm=" + realm
+                + "&realm=/"
                 + "&redirect_uri=" + urlEncodedRedirectUri
                 + "&code=" + authorizationCode;
-        log.debug("Requesting tokens by POST to " + host);
+        log.debug("Requesting tokens by POST to " + parameters.host);
         request.setEntity(new StringEntity(data, ENCODING));
         return request;
     }
@@ -56,6 +74,16 @@ public class IdTokenAndRefreshTokenProvider {
         OidcCredential token = new OidcCredential(TokenProviderUtil.findToken(responseString, "id_token"));
         String refreshToken = TokenProviderUtil.findToken(responseString, "refresh_token");
         return new IdTokenAndRefreshToken(token, refreshToken);
+    }
+
+    @Builder
+    public static class Parameters {
+
+        public final String host;
+        public final String redirectUrl;
+        public final String username;
+        public final String password;
+
     }
 
 }
--- src/test/java/no/nav/brukerdialog/security/oidc/IdTokenAndRefreshTokenProviderTest.java
+++ src/test/java/no/nav/brukerdialog/security/oidc/IdTokenAndRefreshTokenProviderTest.java
@@ -2,6 +2,7 @@ package no.nav.brukerdialog.security.oidc;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.http.client.methods.HttpPost;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -9,6 +10,8 @@ import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.UriInfo;
 import java.net.URI;
 
+import static java.lang.System.clearProperty;
+import static java.lang.System.setProperty;
 import static no.nav.brukerdialog.security.oidc.IdTokenAndRefreshTokenProvider.*;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -17,27 +20,48 @@ import static org.mockito.Mockito.when;
 public class IdTokenAndRefreshTokenProviderTest {
 
     public static final String APPLICATION_NAME_PROPERTY_NAME = "applicationName";
-    public static final String ISSO_HOST_URL_PROPERTY_NAME = "isso-host.url";
-    public static final String ISSO_RP_USER_USERNAME_PROPERTY_NAME = "isso-rp-user.username";
-    public static final String ISSO_RP_USER_PASSWORD_PROPERTY_NAME = "isso-rp-user.password";
+
+    @Before
+    public void setup(){
+        setProperty(APPLICATION_NAME_PROPERTY_NAME, "minapp"); // TODO denne er kjip siden den er statisk på innsiden av klassen
+
+        clearProperty(ISSO_RP_USER_USERNAME_PROPERTY_NAME);
+        clearProperty(ISSO_RP_USER_PASSWORD_PROPERTY_NAME);
+        clearProperty(ISSO_HOST_URL_PROPERTY_NAME);
+    }
 
     @Test
     public void createTokenRequest_fraSystemProperties() throws Exception {
-        System.setProperty(APPLICATION_NAME_PROPERTY_NAME, "minapp");
-        System.setProperty(ISSO_RP_USER_USERNAME_PROPERTY_NAME, "minbruker");
-        System.setProperty(ISSO_RP_USER_PASSWORD_PROPERTY_NAME, "mittpassord");
-        System.setProperty(ISSO_HOST_URL_PROPERTY_NAME, "https://minhost.nav.no");
+        setProperty(ISSO_RP_USER_USERNAME_PROPERTY_NAME, "minbruker");
+        setProperty(ISSO_RP_USER_PASSWORD_PROPERTY_NAME, "mittpassord");
+        setProperty(ISSO_HOST_URL_PROPERTY_NAME, "https://minhost.nav.no");
 
-        HttpPost tokenRequest = new IdTokenAndRefreshTokenProvider().createTokenRequest("abcd", getUriInfo());
+        HttpPost tokenRequest = new IdTokenAndRefreshTokenProvider().createTokenRequest("abcd", getUriInfo("https://host:1234/a/b/c"));
 
         assertThat(getEntityAsString(tokenRequest), equalTo("grant_type=authorization_code&realm=/&redirect_uri=https%3A%2F%2Fhost%3A1234%2Fminapp%2Ftjenester%2Flogin&code=abcd"));
         assertThat(tokenRequest.getHeaders(HttpHeaders.AUTHORIZATION)[0].getValue(), equalTo("Basic bWluYnJ1a2VyOm1pdHRwYXNzb3Jk"));
         assertThat(tokenRequest.getURI().toString(),equalTo("https://minhost.nav.no/access_token"));
     }
 
-    private UriInfo getUriInfo() {
+    @Test
+    public void createTokenRequest_fraParametre() throws Exception {
+        Parameters parameters = Parameters.builder()
+                .redirectUrl("/min/redirect/url")
+                .host("https://minparameterhost.nav.no")
+                .username("minparameterbruker")
+                .password("mittparameterpassord")
+                .build();
+
+        HttpPost tokenRequest = new IdTokenAndRefreshTokenProvider(parameters).createTokenRequest("abcd", getUriInfo("ftp://param:5678/a/b/c"));
+
+        assertThat(getEntityAsString(tokenRequest), equalTo("grant_type=authorization_code&realm=/&redirect_uri=ftp%3A%2F%2Fparam%3A5678%2Fmin%2Fredirect%2Furl&code=abcd"));
+        assertThat(tokenRequest.getHeaders(HttpHeaders.AUTHORIZATION)[0].getValue(), equalTo("Basic bWlucGFyYW1ldGVyYnJ1a2VyOm1pdHRwYXJhbWV0ZXJwYXNzb3Jk"));
+        assertThat(tokenRequest.getURI().toString(),equalTo("https://minparameterhost.nav.no/access_token"));
+    }
+
+    private UriInfo getUriInfo(String baseUri) {
         UriInfo uriInfo = Mockito.mock(UriInfo.class);
-        when(uriInfo.getBaseUri()).thenReturn(URI.create("https://host:1234/a/b/c"));
+        when(uriInfo.getBaseUri()).thenReturn(URI.create(baseUri));
         return uriInfo;
     }
 
