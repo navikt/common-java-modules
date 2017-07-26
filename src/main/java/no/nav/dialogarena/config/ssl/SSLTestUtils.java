6144ada5acda2f5cbf162b164058272103a6429e
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2017-07-26 17:35:26 +0200

    la til ServletContextListener som kan hjelpe til med a disable sertifikat-sjekker

--- src/main/java/no/nav/dialogarena/config/ssl/SSLTestUtils.java
+++ src/main/java/no/nav/dialogarena/config/ssl/SSLTestUtils.java
@@ -1,12 +1,15 @@
 package no.nav.dialogarena.config.ssl;
 
 import lombok.SneakyThrows;
+import org.apache.http.conn.ssl.X509HostnameVerifier;
 
 import javax.net.ssl.*;
+import java.io.IOException;
 import java.security.SecureRandom;
 import java.security.Security;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.util.Properties;
 
 import static no.nav.dialogarena.config.util.Util.setProperty;
 
@@ -14,7 +17,28 @@ public class SSLTestUtils {
 
     public static final SSLContext sslContext = trustAllSSLContext();
     public static final SSLSocketFactory TRUST_ALL_SSL_SOCKET_FACTORY = sslContext.getSocketFactory();
-    public static final HostnameVerifier ALLOW_ALL_HOSTNAME_VERIFIER = (s, sslSession) -> true;
+    public static final X509HostnameVerifier ALLOW_ALL_X509_HOSTNAME_VERIFIER = new X509HostnameVerifier() {
+        @Override
+        public void verify(String host, SSLSocket ssl) throws IOException {
+
+        }
+
+        @Override
+        public void verify(String host, X509Certificate cert) throws SSLException {
+
+        }
+
+        @Override
+        public void verify(String host, String[] cns, String[] subjectAlts) throws SSLException {
+
+        }
+
+        @Override
+        public boolean verify(String s, SSLSession sslSession) {
+            return true;
+        }
+    };
+    public static final HostnameVerifier ALLOW_ALL_HOSTNAME_VERIFIER = ALLOW_ALL_X509_HOSTNAME_VERIFIER;
 
     @SneakyThrows
     private static SSLContext trustAllSSLContext() {
@@ -48,6 +72,16 @@ public class SSLTestUtils {
         CxfCertificateValidationDisabler.init();
         HttpsURLConnection.setDefaultSSLSocketFactory(TRUST_ALL_SSL_SOCKET_FACTORY);
         HttpsURLConnection.setDefaultHostnameVerifier(ALLOW_ALL_HOSTNAME_VERIFIER);
+        SSLContext.setDefault(sslContext);
+        systemPropertyObject(SSLContext.class, sslContext);
+        systemPropertyObject(HostnameVerifier.class, ALLOW_ALL_HOSTNAME_VERIFIER);
+        systemPropertyObject(X509HostnameVerifier.class, ALLOW_ALL_X509_HOSTNAME_VERIFIER);
+    }
+
+    private static <T> void systemPropertyObject(Class<T> aClass, T value) {
+        Properties systemProperties = System.getProperties();
+        systemProperties.put(aClass, value);
+        systemProperties.put(aClass.getName(), value);
     }
 
 }
