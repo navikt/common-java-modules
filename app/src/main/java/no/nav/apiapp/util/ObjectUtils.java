a9785c000482e9972882e77b35d0ff96010d7a26
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2017-08-09 20:00:16 +0200

    la til min() og max() i ObjectUtils

--- app/src/main/java/no/nav/apiapp/util/ObjectUtils.java
+++ app/src/main/java/no/nav/apiapp/util/ObjectUtils.java
@@ -14,4 +14,12 @@ public class ObjectUtils {
         }
     }
 
+    public static <T extends Comparable<T>> T min(T a, T b) {
+        return a != null && (b == null || a.compareTo(b) < 0) ? a : b;
+    }
+
+    public static <T extends Comparable<T>> T max(T a, T b) {
+        return a != null && (b == null || a.compareTo(b) > 0) ? a : b;
+    }
+
 }
--- app/src/test/java/no/nav/apiapp/util/ObjectUtilsTest.java
+++ app/src/test/java/no/nav/apiapp/util/ObjectUtilsTest.java
@@ -2,8 +2,7 @@ package no.nav.apiapp.util;
 
 import org.junit.Test;
 
-import static no.nav.apiapp.util.ObjectUtils.isEqual;
-import static no.nav.apiapp.util.ObjectUtils.notEqual;
+import static no.nav.apiapp.util.ObjectUtils.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -22,6 +21,28 @@ public class ObjectUtilsTest {
     @Test
     public void notEqual_() {
         assertThat(notEqual("a", "b")).isTrue();
+    }
+
+    @Test
+    public void min_() {
+        assertThat(min(1, 2)).isEqualTo(1);
+        assertThat(min(2, 1)).isEqualTo(1);
+        assertThat(min("b", "a")).isEqualTo("a");
+        assertThat(min("a", "b")).isEqualTo("a");
+        assertThat(min(null, "a")).isEqualTo("a");
+        assertThat(min("a", null)).isEqualTo("a");
+        assertThat(min((String)null, null)).isNull();
+    }
+
+    @Test
+    public void max_() {
+        assertThat(max(1, 2)).isEqualTo(2);
+        assertThat(max(2, 1)).isEqualTo(2);
+        assertThat(max("b", "a")).isEqualTo("b");
+        assertThat(max("a", "b")).isEqualTo("b");
+        assertThat(max(null, "a")).isEqualTo("a");
+        assertThat(max("a", null)).isEqualTo("a");
+        assertThat(max((String)null, null)).isNull();
     }
 
 }
