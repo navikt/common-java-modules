6dbb60d19d1eaf6bf3385e13897915cb433cd285
Andreas Lund <andreas.lund@nav.no>
2017-08-21 11:14:18 +0200

    Legger til en "muntlig" måte å formindle dato / år på .

--- src/main/java/no/nav/sbl/dialogarena/time/DateUtil.java
+++ src/main/java/no/nav/sbl/dialogarena/time/DateUtil.java
@@ -3,6 +3,7 @@ package no.nav.sbl.dialogarena.time;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Month;
 import java.time.format.TextStyle;
 import java.util.Locale;
 
@@ -13,6 +14,10 @@ public class DateUtil {
 
     public static String tilKortDato(LocalDate dato) {
         return dato.format(ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String tilMuntligDatoAarFormat(LocalDate dato) {
+        return dato.getDayOfMonth() + ". " + mnd(dato.getMonth()) + " " + dato.getYear();
     }
 
     public static String tilKortDato(LocalDateTime dato) {
@@ -55,6 +60,36 @@ public class DateUtil {
                 return "Lørdag";
             case SUNDAY:
                 return "Søndag";
+        }
+        return "";
+    }
+
+    private static String mnd(Month month) {
+        switch (month) {
+            case JANUARY:
+                return "januar";
+            case FEBRUARY:
+                return "februar";
+            case MARCH:
+                return "mars";
+            case APRIL:
+                return "april";
+            case MAY:
+                return "mai";
+            case JUNE:
+                return "juni";
+            case JULY:
+                return "juli";
+            case AUGUST:
+                return "august";
+            case SEPTEMBER:
+                return "september";
+            case OCTOBER:
+                return "oktober";
+            case NOVEMBER:
+                return "november";
+            case DECEMBER:
+                return "desember";
         }
         return "";
     }
