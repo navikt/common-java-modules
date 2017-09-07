4d400c1308410ad3c757e839c4e1a6bb3cba5577
Andreas Lund <andreas.lund@nav.no>
2017-09-07 10:42:52 +0200

    sonar-fiks.

--- src/main/java/no/nav/sbl/dialogarena/common/web/security/CsrfDoubleSubmitCookieFilter.java
+++ src/main/java/no/nav/sbl/dialogarena/common/web/security/CsrfDoubleSubmitCookieFilter.java
@@ -36,7 +36,8 @@ public class CsrfDoubleSubmitCookieFilter implements Filter {
             }
         } else {
             if (!navCsrfCookieVerdi(request).equals(request.getHeader(CSRF_COOKIE_NAVN))) {
-                LOG.error("Feil i CSRF-sjekk. Bruker du dette filteren må du i frontend sørge for å sende med NAV_CSRF_PROTECTION-cookien som en header med navn NAV_CSRF_PROTECTION og verdien til cookien");
+                LOG.error("Feil i CSRF-sjekk. " +
+                        "Bruker du dette filteren må du i frontend sørge for å sende med NAV_CSRF_PROTECTION-cookien som en header med navn NAV_CSRF_PROTECTION og verdien til cookien");
                 response.sendError(SC_UNAUTHORIZED, "Mangler NAV_CSRF_PROTECTION-cookie!! Du må inkludere cookien-verdien i en header med navn NAV_CSRF_PROTECTION");
             }
         }
