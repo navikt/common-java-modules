a42d64d09dd7bd0b6a731004d4446611f3cde1a3
Jan Erik Robertsen <jan.erik.robertsen@nav.no>
2017-09-11 12:35:34 +0200

    Utvid PepClient med Action og ResourceType

--- src/main/java/no/nav/apiapp/security/PepClient.java
+++ src/main/java/no/nav/apiapp/security/PepClient.java
@@ -2,27 +2,48 @@ package no.nav.apiapp.security;
 
 import lombok.SneakyThrows;
 import no.nav.apiapp.feil.IngenTilgang;
-import no.nav.brukerdialog.security.context.SubjectHandler;
-import no.nav.brukerdialog.security.domain.OidcCredential;
 import no.nav.sbl.dialogarena.common.abac.pep.Pep;
+import no.nav.sbl.dialogarena.common.abac.pep.domain.ResourceType;
+import no.nav.sbl.dialogarena.common.abac.pep.domain.request.Action;
 import no.nav.sbl.dialogarena.common.abac.pep.domain.response.Decision;
-
-import javax.inject.Inject;
+import no.nav.sbl.dialogarena.common.abac.pep.exception.PepException;
 
 
 public class PepClient {
 
     private final Pep pep;
     private final String applicationDomain;
+    private final ResourceType resourceType;
 
+    @Deprecated
     public PepClient(Pep pep, String applicationDomain) {
+        this(pep, applicationDomain, ResourceType.Person);
+    }
+
+    public PepClient(Pep pep, String applicationDomain, ResourceType resourceType) {
         this.pep = pep;
         this.applicationDomain = applicationDomain;
+        this.resourceType = resourceType;
     }
 
     @SneakyThrows
+    @Deprecated
     public String sjekkTilgangTilFnr(String fnr) {
-        if (Decision.Permit == pep.harInnloggetBrukerTilgangTilPerson(fnr, applicationDomain).getBiasedDecision()) {
+        return sjekkLeseTilgangTilFnr(fnr);
+    }
+
+    @SneakyThrows
+    public String sjekkLeseTilgangTilFnr(String fnr) {
+        return sjekkTilgang(fnr, Action.ActionId.READ);
+    }
+
+    @SneakyThrows
+    public String sjekkSkriveTilgangTilFnr(String fnr) {
+        return sjekkTilgang(fnr, Action.ActionId.WRITE);
+    }
+
+    private String sjekkTilgang(String fnr, Action.ActionId action) throws PepException {
+        if (Decision.Permit == pep.harInnloggetBrukerTilgangTilPerson(fnr, applicationDomain, action, resourceType).getBiasedDecision()) {
             return fnr;
         } else {
             throw new IngenTilgang();
