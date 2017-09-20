1181fbb8b850a9374beef458f8ac2a5a728803bc
S151399 <marthe.skogen@nav.no>
2017-09-20 13:44:22 +0200

    fikser sonar-feil

--- src/main/java/no/nav/sbl/dialogarena/common/cxf/CXFMaskTokenLoggingInInterceptor.java
+++ src/main/java/no/nav/sbl/dialogarena/common/cxf/CXFMaskTokenLoggingInInterceptor.java
@@ -40,17 +40,10 @@ public class CXFMaskTokenLoggingInInterceptor extends LoggingInInterceptor{
         message.put(LoggingMessage.ID_KEY, id);
         final LoggingMessage buffer
                 = new LoggingMessage("Inbound Message\n----------------------------", id);
-
-        if (!Boolean.TRUE.equals(message.get(Message.DECOUPLED_CHANNEL_MESSAGE))) {
-            // avoid logging the default responseCode 200 for the decoupled responses
-            Integer responseCode = (Integer)message.get(Message.RESPONSE_CODE);
-            if (responseCode != null) {
-                buffer.getResponseCode().append(responseCode);
-            }
+        if (getResponseCode(message) != null) {
+            buffer.getResponseCode().append(getResponseCode(message));
         }
-
         String encoding = (String)message.get(Message.ENCODING);
-
         if (encoding != null) {
             buffer.getEncoding().append(encoding);
         }
@@ -62,32 +55,12 @@ public class CXFMaskTokenLoggingInInterceptor extends LoggingInInterceptor{
         if (ct != null) {
             buffer.getContentType().append(ct);
         }
-        ObjectMapper objectMapper = new ObjectMapper();
         Object headersObject = message.get(Message.PROTOCOL_HEADERS);
-        TreeMap headers = objectMapper.convertValue(headersObject, TreeMap.class);
-        if (headers != null) {
-            if (maskerToken) {
-                headers = removeCookieFromHttpHeader(headers);
-            }
-            if (!headers.isEmpty()) {
-                buffer.getHeader().append(headers);
-            }
+        if (headersObject != null) {
+            TreeMap headers = getHeaders(headersObject);
+            buffer.getHeader().append(headers);
         }
-        String uri = (String)message.get(Message.REQUEST_URL);
-        if (uri == null) {
-            String address = (String)message.get(Message.ENDPOINT_ADDRESS);
-            uri = (String)message.get(Message.REQUEST_URI);
-            if (uri != null && uri.startsWith("/")) {
-                if (address != null && !address.startsWith(uri)) {
-                    if (address.endsWith("/") && address.length() > 1) {
-                        address = address.substring(0, address.length());
-                    }
-                    uri = address + uri;
-                }
-            } else {
-                uri = address;
-            }
-        }
+        String uri = getUri(message);
         if (uri != null) {
             buffer.getAddress().append(uri);
             String query = (String)message.get(Message.QUERY_STRING);
@@ -95,7 +68,6 @@ public class CXFMaskTokenLoggingInInterceptor extends LoggingInInterceptor{
                 buffer.getAddress().append("?").append(query);
             }
         }
-
         if (!isShowBinaryContent() && isBinaryContent(ct)) {
             buffer.getMessage().append(BINARY_CONTENT_MESSAGE).append('\n');
             log(logger, buffer.toString());
@@ -106,7 +78,6 @@ public class CXFMaskTokenLoggingInInterceptor extends LoggingInInterceptor{
             log(logger, buffer.toString());
             return;
         }
-
         InputStream is = message.getContent(InputStream.class);
         if (is != null) {
             logInputStream(message, is, buffer, encoding, ct);
@@ -119,11 +90,50 @@ public class CXFMaskTokenLoggingInInterceptor extends LoggingInInterceptor{
         log(logger, formatLoggingMessage(buffer));
     }
 
+    private Integer getResponseCode(Message message) {
+        if (!Boolean.TRUE.equals(message.get(Message.DECOUPLED_CHANNEL_MESSAGE))) {
+            // avoid logging the default responseCode 200 for the decoupled responses
+            Integer responseCode = (Integer)message.get(Message.RESPONSE_CODE);
+                return responseCode;
+        }
+        return null;
+    }
+
+    private TreeMap getHeaders(Object headersObject) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        TreeMap headers = objectMapper.convertValue(headersObject, TreeMap.class);
+        if (headers != null) {
+            if (maskerToken) {
+                headers = removeCookieFromHttpHeader(headers);
+            }
+        }
+        return headers;
+    }
+
     private TreeMap removeCookieFromHttpHeader(TreeMap headers) {
         if(headers.containsKey("Cookie"))
         {
             headers.remove("Cookie");
         }
         return headers;
+    }
+
+    private String getUri(Message message) {
+        String uri = (String)message.get(Message.REQUEST_URL);
+        if (uri == null) {
+            String address = (String)message.get(Message.ENDPOINT_ADDRESS);
+            uri = (String)message.get(Message.REQUEST_URI);
+            if (uri != null && uri.startsWith("/")) {
+                if (address != null && !address.startsWith(uri)) {
+                    if (address.endsWith("/") && address.length() > 1) {
+                        address = address.substring(0, address.length());
+                    }
+                    return address + uri;
+                }
+            } else {
+                return address;
+            }
+        }
+        return uri;
     }
 }
