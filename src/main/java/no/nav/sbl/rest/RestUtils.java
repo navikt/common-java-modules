c13d2b2d10853e6b530042862bd24c2b61c36602
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2017-09-27 15:05:15 +0200

    la til metrikker

--- src/main/java/no/nav/sbl/rest/RestUtils.java
+++ src/main/java/no/nav/sbl/rest/RestUtils.java
@@ -2,11 +2,15 @@ package no.nav.sbl.rest;
 
 import lombok.SneakyThrows;
 import no.nav.json.JsonProvider;
+import no.nav.metrics.MetricsFactory;
+import no.nav.metrics.Timer;
 import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.client.JerseyClientBuilder;
 
 import javax.net.ssl.SSLContext;
-import javax.ws.rs.client.Client;
+import javax.ws.rs.client.*;
+import java.io.IOException;
+import java.net.URI;
 import java.util.function.Function;
 
 import static org.glassfish.jersey.client.ClientProperties.*;
@@ -17,6 +21,7 @@ public class RestUtils {
     public static Client createClient() {
         ClientConfig clientConfig = new ClientConfig();
         clientConfig.register(new JsonProvider());
+        clientConfig.register(new MetricsProvider());
         clientConfig.property(FOLLOW_REDIRECTS, false);
         clientConfig.property(CONNECT_TIMEOUT, 5000);
         clientConfig.property(READ_TIMEOUT, 15000);
@@ -34,6 +39,35 @@ public class RestUtils {
         } finally {
             client.close();
         }
+    }
+
+    private static class MetricsProvider implements ClientResponseFilter, ClientRequestFilter {
+
+        public static final String NAME = MetricsProvider.class.getName();
+
+        @Override
+        public void filter(ClientRequestContext clientRequestContext, ClientResponseContext clientResponseContext) throws IOException {
+            Timer timer = (Timer) clientRequestContext.getProperty(NAME);
+            timer
+                    .stop()
+                    .addFieldToReport("httpStatus", clientResponseContext.getStatus())
+                    .report();
+        }
+
+        @Override
+        public void filter(ClientRequestContext clientRequestContext) throws IOException {
+            Timer timer = MetricsFactory.createTimer(timerNavn(clientRequestContext.getUri()));
+            timer.start();
+            clientRequestContext.setProperty(NAME, timer);
+        }
+
+        private String timerNavn(URI uri) {
+            return String.format("rest.client.%s%s",
+                    uri.getHost(),
+                    uri.getPath()
+            );
+        }
+
     }
 
 }
