2a9b253aa36bb3bc357465306d681748fa3e0dae
Fraschetti, Yrjan <Yrjan.Fraschetti@nav.no>
2017-10-09 12:38:51 +0200

    Trakk prometheus versjonsnummer ut i property, Fjernet StringWriter fra PrometheusServlet

--- src/main/java/no/nav/apiapp/metrics/PrometheusServlet.java
+++ src/main/java/no/nav/apiapp/metrics/PrometheusServlet.java
@@ -1,25 +1,20 @@
 package no.nav.apiapp.metrics;
 
 import io.prometheus.client.CollectorRegistry;
-import io.prometheus.client.exporter.MetricsServlet;
 import io.prometheus.client.hotspot.DefaultExports;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.StringWriter;
 
 public class PrometheusServlet extends io.prometheus.client.exporter.MetricsServlet {
 
     protected void doGet(final HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        StringWriter writer = new StringWriter();
-
         DefaultExports.initialize();
         io.prometheus.client.exporter.common.TextFormat.write004(
-                writer,
+                response.getWriter(),
                 CollectorRegistry.defaultRegistry.metricFamilySamples()
         );
-        response.getWriter().write(writer.toString());
     }
 }
