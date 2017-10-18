0b298b7d682522cbff4ec9bd0293e7b17300a39c
Peder Korsveien <peder.korsveien@nav.no>
2017-10-18 08:19:33 +0200

    Gjør Kotlin-modul valgfri

--- src/main/java/no/nav/json/JsonProvider.java
+++ src/main/java/no/nav/json/JsonProvider.java
@@ -1,5 +1,6 @@
 package no.nav.json;
 
+import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
@@ -8,11 +9,15 @@ import com.fasterxml.jackson.module.kotlin.KotlinModule;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 
+import java.util.Collections;
+import java.util.List;
+
 import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
 import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
 import static com.fasterxml.jackson.databind.DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT;
 import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
 import static com.fasterxml.jackson.databind.SerializationFeature.INDENT_OUTPUT;
+import static java.util.Collections.singletonList;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static no.nav.json.DateConfiguration.dateModule;
 import static no.nav.sbl.util.EnvironmentUtils.EnviromentClass.T;
@@ -26,11 +31,20 @@ public class JsonProvider extends JacksonJaxbJsonProvider {
         setMapper(createObjectMapper());
     }
 
+    public JsonProvider(List<Module> modules) {
+        ObjectMapper objectMapper = createObjectMapper();
+        modules.forEach(objectMapper::registerModule);
+        setMapper(objectMapper);
+    }
+
+    public static JsonProvider jsonProviderWithKotlin() {
+        return new JsonProvider(singletonList(new KotlinModule()));
+    }
+
     public static ObjectMapper createObjectMapper() {
         ObjectMapper objectMapper = new ObjectMapper()
                 .registerModule(new Jdk8Module())
                 .registerModule(dateModule())
-                .registerModule(new KotlinModule())
                 .configure(FAIL_ON_UNKNOWN_PROPERTIES, false)
                 .configure(ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true)
                 ;
@@ -48,6 +62,5 @@ public class JsonProvider extends JacksonJaxbJsonProvider {
 
         return objectMapper;
     }
-
 
 }
