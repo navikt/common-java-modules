5edaaccee8b291a988a93aa2836c8d196655d4d4
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2017-10-24 20:20:32 +0200

    la til combineOptional()

--- src/main/java/no/nav/sbl/util/FunctionalUtils.java
+++ src/main/java/no/nav/sbl/util/FunctionalUtils.java
@@ -5,10 +5,8 @@ import no.nav.sbl.util.fn.UnsafeConsumer;
 import no.nav.sbl.util.fn.UnsafeFunction;
 import no.nav.sbl.util.fn.UnsafeSupplier;
 
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Supplier;
+import java.util.Optional;
+import java.util.function.*;
 
 public class FunctionalUtils {
 
@@ -26,6 +24,10 @@ public class FunctionalUtils {
 
     public static <T, R> Function<T, R> sneakyFunction(UnsafeFunction<T, R> function) {
         return function;
+    }
+
+    public static <A, B, R> Function<A, Optional<R>> combineOptional(Optional<B> optionalB, BiFunction<A, B, R> biConsumer) {
+        return a -> optionalB.map(b -> biConsumer.apply(a, b));
     }
 
 }
--- src/test/java/no/nav/sbl/util/FunctionalUtilsTest.java
+++ src/test/java/no/nav/sbl/util/FunctionalUtilsTest.java
@@ -1,21 +1,27 @@
 package no.nav.sbl.util;
 
+import lombok.Builder;
+import lombok.Value;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Optional;
 
 import static java.util.Arrays.asList;
+import static java.util.Optional.*;
 import static java.util.Optional.ofNullable;
+import static no.nav.sbl.util.FunctionalUtils.combineOptional;
 import static no.nav.sbl.util.FunctionalUtils.sneaky;
 import static no.nav.sbl.util.FunctionalUtils.sneakyFunction;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class FunctionalUtilsTest {
 
     @Test
     public void sneaky_lambdaer_og_metodereferanser_kompilerer_utvetydig() {
         new HashMap<String, String>().forEach(sneaky(this::biConsumerMedCheckedException));
-        new HashMap<String, String>().forEach(sneaky((k, v) -> biConsumerMedCheckedException(k,v)));
+        new HashMap<String, String>().forEach(sneaky((k, v) -> biConsumerMedCheckedException(k, v)));
 
         asList().forEach(sneaky(this::consumerMedCheckedException));
         asList().forEach(sneaky((k) -> consumerMedCheckedException(k)));
@@ -25,6 +31,24 @@ public class FunctionalUtilsTest {
 
         ofNullable("").map(this::alltidNull).map(sneakyFunction((s) -> functionMedCheckedException(s)));
         ofNullable("").map(this::alltidNull).map(sneakyFunction(this::functionMedCheckedException));
+    }
+
+    @Test
+    public void reducer_kan_kombinere_optionals() {
+        Optional<Boolean> aBoolean = of(true);
+        Optional<String> string = of("string");
+        Optional<String> emptyString = empty();
+
+        assertThat(of(TestObject.builder())
+                .flatMap(combineOptional(aBoolean, TestObject.TestObjectBuilder::aBoolean))
+                .flatMap(combineOptional(string, TestObject.TestObjectBuilder::string))
+                .map(TestObject.TestObjectBuilder::build)
+        ).hasValue(new TestObject(true, "string"));
+
+        assertThat(of(TestObject.builder())
+                .flatMap(combineOptional(aBoolean, TestObject.TestObjectBuilder::aBoolean))
+                .flatMap(combineOptional(emptyString, TestObject.TestObjectBuilder::string))
+        ).isEmpty();
     }
 
     @SuppressWarnings("unused")
@@ -48,6 +72,13 @@ public class FunctionalUtilsTest {
 
     private <T> T alltidNull(T t) {
         return null;
+    }
+
+    @Value
+    @Builder
+    private static class TestObject {
+        private boolean aBoolean;
+        private String string;
     }
 
 }
