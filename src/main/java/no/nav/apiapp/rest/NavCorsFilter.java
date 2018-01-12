7463e7f108f4471b6ca8a05b90a7ff6f3ff2f1b3
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2018-01-12 10:30:02 +0100

    krever at man konfigurerer subdomener for å unngå at f.eks. evilnav.no godtas hvis man tillater nav.no

--- src/main/java/no/nav/apiapp/rest/NavCorsFilter.java
+++ src/main/java/no/nav/apiapp/rest/NavCorsFilter.java
@@ -20,7 +20,6 @@ public class NavCorsFilter implements Filter {
     public static final String CORS_ALLOWED_ORIGINS = "cors.allowed.origins";
     public static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
 
-    public static final List<String> ALLOWED_ORIGINS = getAllowedOrigins();
     public static final String ALLOWED_METHODS = Arrays.asList(
             "GET",
             "HEAD",
@@ -31,13 +30,23 @@ public class NavCorsFilter implements Filter {
             "OPTIONS"
     ).stream().collect(joining(", "));
 
+    public List<String> ALLOWED_ORIGINS = getAllowedOrigins();
+
     static List<String> getAllowedOrigins() {
         return getOptionalProperty(CORS_ALLOWED_ORIGINS)
                 .map(string -> string.split(","))
                 .map(Stream::of)
                 .orElseGet(Stream::empty)
                 .map(String::trim)
+                .map(NavCorsFilter::validerSubdomene)
                 .collect(Collectors.toList());
+    }
+
+    private static String validerSubdomene(String subdomene) {
+        if (subdomene.charAt(0) != '.') {
+            throw new IllegalArgumentException("tillatt skal være subdomene, f.eks. '.nav.no' ikke 'nav.no'");
+        }
+        return subdomene;
     }
 
     @Override
@@ -47,7 +56,6 @@ public class NavCorsFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         String originHeader = httpServletRequest.getHeader(ORIGIN);
 
@@ -64,9 +72,9 @@ public class NavCorsFilter implements Filter {
         return validOrigin(originHeader, ALLOWED_ORIGINS);
     }
 
-    static boolean validOrigin(String originHeader, List<String> allowedOrigins) {
+    static boolean validOrigin(String originHeader, List<String> allowedSubDomains) {
         return StringUtils.notNullOrEmpty(originHeader) &&
-                allowedOrigins.stream().filter(StringUtils::notNullOrEmpty).anyMatch(originHeader::endsWith);
+                allowedSubDomains.stream().filter(StringUtils::notNullOrEmpty).anyMatch(originHeader::endsWith);
     }
 
     @Override
--- src/test/java/no/nav/apiapp/rest/NavCorsFilterTest.java
+++ src/test/java/no/nav/apiapp/rest/NavCorsFilterTest.java
@@ -5,10 +5,9 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.Collections;
 
-import static no.nav.apiapp.rest.NavCorsFilter.CORS_ALLOWED_ORIGINS;
-import static no.nav.apiapp.rest.NavCorsFilter.getAllowedOrigins;
-import static no.nav.apiapp.rest.NavCorsFilter.validOrigin;
+import static no.nav.apiapp.rest.NavCorsFilter.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 class NavCorsFilterTest {
@@ -19,32 +18,40 @@ class NavCorsFilterTest {
         assertInvalidOrigin("", null);
         assertInvalidOrigin(null, null, null);
         assertInvalidOrigin("abcd.nav.no");
-        assertInvalidOrigin("nav.no", "evil.com");
+        assertInvalidOrigin("evil.com", ".nav.no");
         assertInvalidOrigin("evil.com", "", null);
+        assertInvalidOrigin("abcd.nav.no", ".nav.noo");
 
-        assertValidOrigin("abcd.nav.no", "nav.no");
-        assertValidOrigin("abcd.nav.no", "nav.no");
-        assertValidOrigin("abcd.nav.no", "oera.no", "nav.no");
-        assertValidOrigin("abcd.nav.no", "oera.no", "", null, "nav.no");
+        assertValidOrigin("abcd.nav.no", ".nav.no");
+        assertValidOrigin("abcd.nav.no", ".nav.no");
+        assertValidOrigin("abcd.nav.no", ".oera.no", ".nav.no");
+        assertValidOrigin("abcd.nav.no", ".oera.no", "", null, ".nav.no");
     }
 
-    private void assertValidOrigin(String origin, String... validOrigins) {
-        assertThat(validOrigin(origin, Arrays.asList(validOrigins))).isTrue();
+    private void assertValidOrigin(String origin, String... validSubDomains) {
+        assertThat(validOrigin(origin, Arrays.asList(validSubDomains))).isTrue();
     }
 
-    private void assertInvalidOrigin(String origin, String... validOrigins) {
-        assertThat(validOrigin(origin, validOrigins != null ? Arrays.asList(validOrigins) : Collections.emptyList())).isFalse();
+    private void assertInvalidOrigin(String origin, String... validSubDomains) {
+        assertThat(validOrigin(origin, validSubDomains != null ? Arrays.asList(validSubDomains) : Collections.emptyList())).isFalse();
     }
 
     @Test
     public void hentKommaseparertListe() {
+        System.clearProperty(CORS_ALLOWED_ORIGINS);
         assertThat(getAllowedOrigins()).isEmpty();
         System.setProperty(CORS_ALLOWED_ORIGINS, "");
         assertThat(getAllowedOrigins()).isEmpty();
-        System.setProperty(CORS_ALLOWED_ORIGINS, "nav.no,oera.no");
-        assertThat(getAllowedOrigins()).containsExactlyInAnyOrder("nav.no", "oera.no");
-        System.setProperty(CORS_ALLOWED_ORIGINS, " nav.no, oera.no ");
-        assertThat(getAllowedOrigins()).containsExactlyInAnyOrder("nav.no", "oera.no");
+        System.setProperty(CORS_ALLOWED_ORIGINS, ".nav.no,.oera.no");
+        assertThat(getAllowedOrigins()).containsExactlyInAnyOrder(".nav.no", ".oera.no");
+        System.setProperty(CORS_ALLOWED_ORIGINS, " .nav.no, .oera.no ");
+        assertThat(getAllowedOrigins()).containsExactlyInAnyOrder(".nav.no", ".oera.no");
+    }
+
+    @Test
+    public void krevSubdomene() {
+        System.setProperty(CORS_ALLOWED_ORIGINS, "ikke.subdomene.no");
+        assertThatThrownBy(NavCorsFilter::getAllowedOrigins).isInstanceOf(IllegalArgumentException.class);
     }
 
 }
