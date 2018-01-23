17390350373a2e23b9ba4c0fc8b14c0fbe5c90de
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2018-01-23 19:12:00 +0100

    PUS-9 utvidet ping-metadata med id

--- src/test/java/no/nav/sbl/dialogarena/types/PingableTest.java
+++ src/test/java/no/nav/sbl/dialogarena/types/PingableTest.java
@@ -33,8 +33,8 @@ public class PingableTest {
 
     @Before
     public void simulatePingResponses() {
-        PingMetadata sykMetadata = new PingMetadata("sykTjeneste_v1", "en feilende tjeneste", false);
-        PingMetadata friskMetadata = new PingMetadata("friskTjeneste_v1", "en frisk tjeneste", false);
+        PingMetadata sykMetadata = new PingMetadata("sykTjeneste","sykTjeneste_v1", "en feilende tjeneste", false);
+        PingMetadata friskMetadata = new PingMetadata("friskTjeneste","friskTjeneste_v1", "en frisk tjeneste", false);
 
         when(sykTjeneste.ping()).thenReturn(Ping.feilet(sykMetadata, new IOException("tjenesten svarer ikke")));
         when(friskTjeneste.ping()).thenReturn(Ping.lyktes(friskMetadata));
@@ -51,7 +51,9 @@ public class PingableTest {
         List<Ping> nonWorking = on(asList(friskTjeneste, sykTjeneste)).map(pingResult()).filter(not(vellykketPing())).collect();
 
         assertThat(nonWorking, hasSize(1));
-        assertThat(nonWorking.get(0).getMetadata().getEndepunkt(), is("sykTjeneste_v1"));
+        PingMetadata pingMetadata = nonWorking.get(0).getMetadata();
+        assertThat(pingMetadata.getId(), is("sykTjeneste"));
+        assertThat(pingMetadata.getEndepunkt(), is("sykTjeneste_v1"));
         boolean isCorrectException = nonWorking.get(0).getFeil() instanceof IOException;
         assertTrue(isCorrectException);
     }
