3ef7a4a85afff04397dd9e773abe0ed809226be3
Peder Korsveien <peder.korsveien@nav.no>
2018-02-12 15:15:59 +0100

    PUS-115 Legg til truststore-path i selftest hvis den er satt

--- src/main/java/no/nav/apiapp/selftest/impl/TruststoreHelsesjekk.java
+++ src/main/java/no/nav/apiapp/selftest/impl/TruststoreHelsesjekk.java
@@ -3,6 +3,7 @@ package no.nav.apiapp.selftest.impl;
 import no.nav.apiapp.selftest.Helsesjekk;
 import no.nav.apiapp.selftest.HelsesjekkMetadata;
 
+import static no.nav.apiapp.ApiApp.NAV_TRUSTSTORE_PATH;
 import static no.nav.apiapp.ApiApp.TRUSTSTORE;
 import static no.nav.sbl.util.EnvironmentUtils.getOptionalProperty;
 
@@ -17,7 +18,7 @@ public class TruststoreHelsesjekk implements Helsesjekk {
     public HelsesjekkMetadata getMetadata() {
         return new HelsesjekkMetadata(
                 "truststore",
-                null,
+                getOptionalProperty(NAV_TRUSTSTORE_PATH).orElse("N/A"),
                 "Sjekker at truststore er satt",
                 true
         );
