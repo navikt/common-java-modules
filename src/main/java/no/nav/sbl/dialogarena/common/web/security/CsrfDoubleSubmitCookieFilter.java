0c1dce3a5d35594c586b5247020c60b2dc4590b7
Sneha Desai <sneha.desai@nav.no>
2018-02-28 13:04:21 +0100

    FO-160:Legge til NAV_CSRF_PROTECTION cookie

--- src/main/java/no/nav/sbl/dialogarena/common/web/security/CsrfDoubleSubmitCookieFilter.java
+++ src/main/java/no/nav/sbl/dialogarena/common/web/security/CsrfDoubleSubmitCookieFilter.java
@@ -7,7 +7,8 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Stream;
 
 import static java.util.Arrays.stream;
 import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
@@ -40,32 +41,28 @@ public class CsrfDoubleSubmitCookieFilter implements Filter {
                 if (request.getCookies() == null || stream(request.getCookies()).noneMatch(cookie -> cookie.getName().equals(CSRF_COOKIE_NAVN))) {
                     response.addCookie(createCsrfProtectionCookie(request));
                 }
-            } else {
-                if (!navCsrfCookieVerdi(request).equals(request.getHeader(CSRF_COOKIE_NAVN))) {
-                    LOG.error("Feil i CSRF-sjekk. " +
-                            "Bruker du dette filteret må du i frontend sørge for å sende med NAV_CSRF_PROTECTION-cookien som en header med navn NAV_CSRF_PROTECTION og verdien til cookien. " +
-                            "Er headeren satt? " + isNotBlank(request.getHeader(CSRF_COOKIE_NAVN)));
-                    response.sendError(SC_UNAUTHORIZED, "Mangler NAV_CSRF_PROTECTION-cookie!! Du må inkludere cookie-verdien i en header med navn NAV_CSRF_PROTECTION");
-                    return;
-                }
+            } else if (!cookieMatcherHeader(request)) {
+                LOG.error("Feil i CSRF-sjekk. " +
+                        "Bruker du dette filteret må du i frontend sørge for å sende med NAV_CSRF_PROTECTION-cookien som en header med navn NAV_CSRF_PROTECTION og verdien til cookien. " +
+                        "Er headeren satt? " + isNotBlank(request.getHeader(CSRF_COOKIE_NAVN)));
+                response.sendError(SC_UNAUTHORIZED, "Mangler NAV_CSRF_PROTECTION-cookie!! Du må inkludere cookie-verdien i en header med navn NAV_CSRF_PROTECTION");
+                return;
             }
         }
         filterChain.doFilter(request, response);
     }
 
-    private String navCsrfCookieVerdi(HttpServletRequest request) {
-        return stream(request.getCookies())
+    private boolean cookieMatcherHeader(HttpServletRequest request) {
+        return streamNullSafe(request.getCookies())
                 .filter(cookie -> cookie.getName().equals(CSRF_COOKIE_NAVN))
+                .map(Cookie::getValue)
                 .findFirst()
-                .orElseThrow(this::manglerCsrfCookie)
-                .getValue();
+                .map(v -> v.equals(request.getHeader(CSRF_COOKIE_NAVN)))
+                .orElse(false);
     }
 
-    private RuntimeException manglerCsrfCookie() {
-        return new RuntimeException("Mangler NAV_CSRF_PROTECTION-cookie. " +
-                "Det betyr at brukeren ikke har gjort en GET-request til en applikasjon med dette filteret før POST/PUT/DELETE, " +
-                "eller at brukeren har slettet tokenet fra browseren."
-        );
+    private static <T> Stream<T> streamNullSafe(T[] cookies) {
+        return cookies != null ? stream(cookies) : Stream.empty();
     }
 
     private Cookie createCsrfProtectionCookie(HttpServletRequest request) {
