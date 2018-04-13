500f8af1d043b6411d195a3f6b41fcddcdcaa770
Kim Tore Jensen <kimtjen@gmail.com>
2018-04-13 10:34:31 +0200

    rest: use mocked HTTP server instead of live data
    This commit removes the requirement of having an internal Fasit server
    up and running in order to complete the tests.

--- rest/src/test/java/no/nav/sbl/rest/MetricsIntegrationTest.java
+++ rest/src/test/java/no/nav/sbl/rest/MetricsIntegrationTest.java
@@ -1,16 +1,25 @@
 package no.nav.sbl.rest;
 
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import org.apache.commons.io.IOUtils;
+import org.junit.Rule;
 import org.junit.Test;
 
 import java.net.ServerSocket;
 import java.nio.charset.Charset;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.givenThat;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static java.lang.System.setProperty;
 import static no.nav.metrics.handlers.SensuHandler.SENSU_CLIENT_PORT;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MetricsIntegrationTest {
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(58089);
 
     @Test
     public void metrikkerSendesTilSensuSocket() throws Exception {
@@ -18,12 +27,11 @@ public class MetricsIntegrationTest {
             sensuServerSocketMock.setSoTimeout(5000);
             setProperty(SENSU_CLIENT_PORT, Integer.toString(sensuServerSocketMock.getLocalPort()));
 
-            RestUtils.withClient(c -> c.target("http://fasit.adeo.no/a/b/c").request().get());
+            givenThat(get(urlEqualTo("/")).willReturn(aResponse().withStatus(200)));
+            RestUtils.withClient(c -> c.target("http://localhost:58089").request().get());
 
             String sensuMetricMessage = IOUtils.toString(sensuServerSocketMock.accept().getInputStream(), Charset.defaultCharset());
             assertThat(sensuMetricMessage).isNotEmpty();
         }
     }
-
-
 }
