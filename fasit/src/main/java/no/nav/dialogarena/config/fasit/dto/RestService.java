b453a60e0e1baeaf96e1523e3ed3712473f04cf4
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2018-04-23 10:58:12 +0200

    added FasitUtils.getRestService()

---
+++ fasit/src/main/java/no/nav/dialogarena/config/fasit/dto/RestService.java
@@ -0,0 +1,33 @@
+package no.nav.dialogarena.config.fasit.dto;
+
+import javax.ws.rs.core.GenericType;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RestService {
+
+    public static final GenericType<List<RestService>> LIST_TYPE = new GenericType<List<RestService>>() {};
+
+    private String alias;
+    private Map<String, String> scope = new HashMap<>();
+    private Map<String, String> properties = new HashMap<>();
+    private Map<String, String> exposedby = new HashMap<>();
+
+    public String getUrl() {
+        return properties.get("url");
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public String getEnvironment() {
+        return scope.get("environment");
+    }
+
+    public String getApplication() {
+        return exposedby.get("application");
+    }
+
+}
