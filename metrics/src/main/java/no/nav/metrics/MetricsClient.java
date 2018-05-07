e38a3d5121c6b1533a2569c41cfa6125268ac52e
Nicklas Utgaard <nicklas.utgaard@nav.no>
2018-05-07 13:29:05 +0200

    [KAIZEN-0] Disable systemtags
    Legger til mulighet for a fjerne automagisk systemTags.
    fo-frontendlogger vil veldig gjerne sette `application` selv.

--- metrics/src/main/java/no/nav/metrics/MetricsClient.java
+++ metrics/src/main/java/no/nav/metrics/MetricsClient.java
@@ -10,16 +10,18 @@ import static java.lang.Boolean.parseBoolean;
 import static java.lang.System.getProperty;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static no.nav.metrics.MetricsFactory.DISABLE_METRICS_REPORT_KEY;
+import static no.nav.metrics.MetricsFactory.DISABLE_SYSTEM_TAGS_KEY;
 
 
 public class MetricsClient {
     public static final Boolean DISABLE_METRICS_REPORT = parseBoolean(getProperty(DISABLE_METRICS_REPORT_KEY, "false"));
+    public static final Boolean DISABLE_SYSTEM_TAGS = parseBoolean(getProperty(DISABLE_SYSTEM_TAGS_KEY, "false"));
     private final Map<String, String> systemTags = new HashMap<>();
     private final SensuHandler sensuHandler;
 
     public MetricsClient() {
         addSystemPropertiesToTags();
-        sensuHandler = new SensuHandler(systemTags.get("application"));;
+        sensuHandler = new SensuHandler(systemTags.get("application"));
     }
 
     private void addSystemPropertiesToTags() {
@@ -29,7 +31,9 @@ public class MetricsClient {
     }
 
     void report(String metricName, Map<String, Object> fields, Map<String, String> tagsFromMetric, long timestampInMilliseconds) {
-        tagsFromMetric.putAll(systemTags);
+        if (!DISABLE_SYSTEM_TAGS) {
+            tagsFromMetric.putAll(systemTags);
+        }
         if (!DISABLE_METRICS_REPORT) {
             long timestamp = MILLISECONDS.toNanos(timestampInMilliseconds);
             String output = InfluxHandler.createLineProtocolPayload(metricName, tagsFromMetric, fields, timestamp);
