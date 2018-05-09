6c05262fb0a944c2b8e354d1e9775ae1a51fec64
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2018-05-09 20:54:55 +0200

    more local dns/ips that should not be proxied

--- test/src/main/java/no/nav/sbl/dialogarena/test/WebProxyConfigurator.java
+++ test/src/main/java/no/nav/sbl/dialogarena/test/WebProxyConfigurator.java
@@ -9,7 +9,7 @@ public class WebProxyConfigurator {
     @SneakyThrows
     public static void setupWebProxy() {
         String localHostName = InetAddress.getLocalHost().getCanonicalHostName();
-        System.setProperty("http.nonProxyHosts", localHostName + "|*.155.55.|*.192.168.|*.10.|*.local|*.rtv.gov|*.adeo.no|*.nav.no|*.aetat.no|*.devillo.no|*.oera.no");
+        System.setProperty("http.nonProxyHosts", localHostName + "|localhost|127.0.0.1|0.0.0.0|*.155.55.|*.192.168.|*.10.|*.local|*.rtv.gov|*.adeo.no|*.nav.no|*.aetat.no|*.devillo.no|*.oera.no");
         System.setProperty("http.proxyHost", "webproxy-utvikler.nav.no");
         System.setProperty("http.proxyPort", "8088");
         System.setProperty("https.proxyHost", "webproxy-utvikler.nav.no");
