5e0fb285d571db8aa69737029958042e794b9112
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2018-05-16 21:26:44 +0200

    Fixing resolving of classpath resources
    When we embed jetty, some classes might be loaded by the default WebAppClassLoader and some by the system class loader. These classes will be incompatible with each other. Also, Jetty does not consult the classloader of the webapp when resolving resources such as the swagger-ui. We mitigate both these problems by installing an empty classloader that will always defer to the system classloader.
    
    PUS-161

--- api-app/src/test/java/no/nav/fo/apiapp/ApiAppTestMain.java
+++ api-app/src/test/java/no/nav/fo/apiapp/ApiAppTestMain.java
@@ -1,33 +1,12 @@
 package no.nav.fo.apiapp;
 
-import no.nav.common.auth.openam.sbs.OpenAmConfig;
-import no.nav.dialogarena.config.fasit.FasitUtils;
-import no.nav.dialogarena.config.fasit.ServiceUser;
-import no.nav.sbl.dialogarena.common.cxf.StsSecurityConstants;
 import no.nav.testconfig.ApiAppTest;
-
-import static no.nav.brukerdialog.security.oidc.provider.AzureADB2CConfig.AZUREAD_B2C_DISCOVERY_URL_PROPERTY_NAME;
-import static no.nav.brukerdialog.security.oidc.provider.AzureADB2CConfig.AZUREAD_B2C_EXPECTED_AUDIENCE_PROPERTY_NAME;
-import static no.nav.dialogarena.config.util.Util.setProperty;
 
 public class ApiAppTestMain {
 
     public static void main(String[] args) throws Exception {
         ApiAppTest.setupTestContext();
-
-        String securityTokenService = FasitUtils.getBaseUrl("securityTokenService");
-        ServiceUser srvveilarbdemo = FasitUtils.getServiceUser("srvveilarbdemo", "veilarbdemo");
-
-        setProperty(StsSecurityConstants.STS_URL_KEY, securityTokenService);
-        setProperty(StsSecurityConstants.SYSTEMUSER_USERNAME, srvveilarbdemo.getUsername());
-        setProperty(StsSecurityConstants.SYSTEMUSER_PASSWORD, srvveilarbdemo.getPassword());
-
-        setProperty(OpenAmConfig.OPENAM_RESTURL, "https://itjenester-" + FasitUtils.getDefaultTestEnvironment().toString() + ".oera.no/esso");
-
-        ServiceUser azureADClientId = FasitUtils.getServiceUser("aad_b2c_clientid", "veilarbdemo");
-        setProperty(AZUREAD_B2C_DISCOVERY_URL_PROPERTY_NAME, FasitUtils.getBaseUrl("aad_b2c_discovery"));
-        setProperty(AZUREAD_B2C_EXPECTED_AUDIENCE_PROPERTY_NAME, azureADClientId.username);
-
+        JettyTest.setupContext();
         ApiAppMain.main();
     }
 
