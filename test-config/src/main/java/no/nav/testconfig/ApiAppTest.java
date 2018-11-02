3f9cbd2321b8b241be3cffe0d5df9f458aefe436
Peder Korsveien <peder.korsveien@nav.no>
2018-11-02 17:26:51 +0100

    Comment out setupNavTrustStore

--- test-config/src/main/java/no/nav/testconfig/ApiAppTest.java
+++ test-config/src/main/java/no/nav/testconfig/ApiAppTest.java
@@ -66,7 +66,7 @@ public class ApiAppTest {
 
         if (isUtviklerImage()) {
             WebProxyConfigurator.setupWebProxy();
-            setupNavTrustStore();
+//            setupNavTrustStore();
         }
     }
 
