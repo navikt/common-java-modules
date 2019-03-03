470295c2b72d279651b4210c47fa8451206f60c4
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2019-03-03 20:07:54 +0100

    made AuthorizationRequestBuilder more easily configurable

--- oidc-security/src/main/java/no/nav/brukerdialog/security/jaspic/AuthorizationRequestBuilder.java
+++ oidc-security/src/main/java/no/nav/brukerdialog/security/jaspic/AuthorizationRequestBuilder.java
@@ -5,18 +5,26 @@ import java.math.BigInteger;
 import java.net.URLEncoder;
 import java.security.SecureRandom;
 
-import static no.nav.brukerdialog.security.Constants.*;
-
 public class AuthorizationRequestBuilder {
 
     private static final SecureRandom random = new SecureRandom();
 
     private final String scope = "openid";
+    private final String issoHostUrl;
+    private final String clientId;
+    private final String redirectUrl;
     private boolean useKerberos = true;
     private String stateIndex;
 
-
     public AuthorizationRequestBuilder() {
+        this(AuthorizationRequestBuilderConfig.resolveFromSystemProperties());
+    }
+
+    public AuthorizationRequestBuilder(AuthorizationRequestBuilderConfig authorizationRequestBuilderConfig) {
+        this.issoHostUrl = authorizationRequestBuilderConfig.getIssoHostUrl();
+        this.clientId = authorizationRequestBuilderConfig.getClientId();
+        this.redirectUrl = authorizationRequestBuilderConfig.getRedirectUrl();
+
         byte bytes[] = new byte[20];
         random.nextBytes(bytes);
         stateIndex = "state_" + new BigInteger(1, bytes).toString(16);
@@ -37,11 +45,11 @@ public class AuthorizationRequestBuilder {
                 ? "session=winssochain&authIndexType=service&authIndexValue=winssochain&"
                 : "";
         return String.format("%s/authorize?" + kerberosTrigger + "response_type=code&scope=%s&client_id=%s&state=%s&redirect_uri=%s",
-                getIssoHostUrl(),
+                issoHostUrl,
                 scope,
-                URLEncoder.encode(getIssoRpUserUsername(), "UTF-8"),
+                URLEncoder.encode(clientId, "UTF-8"),
                 state,
-                URLEncoder.encode(getOidcRedirectUrl(), "UTF-8")
+                URLEncoder.encode(redirectUrl, "UTF-8")
         );
     }
 }
