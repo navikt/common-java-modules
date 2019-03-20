c78a368dac6eadd30c29120005c0a8e34d444b90
matiasvinjevoll <matias.vinjevoll@nav.no>
2019-03-20 14:49:41 +0100

    Support for authorizing security level of external users
    Security level of external users can be authorized when ApiAppConfigurator is configured with validateAzureAdExternalUserTokens. Also support granular control of security level on different paths with customSecurityLevelForExternalUsers.

--- api-app/src/main/java/no/nav/apiapp/security/SecurityLevelAuthorizationModule.java
+++ api-app/src/main/java/no/nav/apiapp/security/SecurityLevelAuthorizationModule.java
@@ -2,36 +2,39 @@ package no.nav.apiapp.security;
 
 import no.nav.brukerdialog.security.oidc.OidcTokenUtils;
 import no.nav.common.auth.AuthorizationModule;
+import no.nav.common.auth.SecurityLevel;
 import no.nav.common.auth.SsoToken;
 import no.nav.common.auth.Subject;
 import no.nav.common.auth.openam.sbs.OpenAmUtils;
 
 import javax.servlet.http.HttpServletRequest;
 
-import static no.nav.common.auth.SsoToken.Type.EKSTERN_OPENAM;
-
 public class SecurityLevelAuthorizationModule implements AuthorizationModule {
 
-    private final int minimumLevel;
+    private final SecurityLevel minimumLevel;
 
-    public SecurityLevelAuthorizationModule(int minimumLevel) {
+    public SecurityLevelAuthorizationModule(SecurityLevel minimumLevel) {
         this.minimumLevel = minimumLevel;
     }
 
     @Override
     public boolean authorized(Subject subject, HttpServletRequest httpServletRequest) {
-        return subject != null && getSecurityLevel(subject) >= minimumLevel;
+        return authorized(subject, httpServletRequest, minimumLevel);
     }
 
-    private int getSecurityLevel(Subject subject) {
+    public static boolean authorized(Subject subject, HttpServletRequest httpServletRequest, SecurityLevel minimumLevel) {
+        return subject != null && getSecurityLevel(subject).getSecurityLevel() >= minimumLevel.getSecurityLevel();
+    }
+
+    public static SecurityLevel getSecurityLevel(Subject subject) {
         SsoToken ssoToken = subject.getSsoToken();
         switch (ssoToken.getType()) {
             case OIDC:
-                return OidcTokenUtils.getOidcSecurityLevel(ssoToken).getSecurityLevel();
+                return OidcTokenUtils.getOidcSecurityLevel(ssoToken);
             case EKSTERN_OPENAM:
-                return OpenAmUtils.getSecurityLevel(ssoToken).getSecurityLevel();
+                return OpenAmUtils.getSecurityLevel(ssoToken);
             default:
-                return Integer.MIN_VALUE;
+                return SecurityLevel.Ukjent;
         }
     }
 
