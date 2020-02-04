ed2669961b17dc74e156222b9823c0a453caf096
Alexander Gård <alex.gaard@hotmail.com>
2020-02-04 15:38:02 +0100

    Slett ubrukt kode

--- api-app/src/main/java/no/nav/apiapp/config/ApiAppConfigurator.java
+++ api-app/src/main/java/no/nav/apiapp/config/ApiAppConfigurator.java
@@ -2,23 +2,15 @@ package no.nav.apiapp.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import no.nav.apiapp.auth.OidcAuthenticatorConfig;
-import no.nav.brukerdialog.security.domain.IdentType;
-import no.nav.brukerdialog.security.oidc.provider.AzureADB2CConfig;
-import no.nav.brukerdialog.security.oidc.provider.OidcProvider;
-import no.nav.brukerdialog.security.oidc.provider.SecurityTokenServiceOidcProviderConfig;
-import no.nav.common.auth.AuthorizationModule;
-import no.nav.common.auth.SecurityLevel;
-import no.nav.common.auth.openam.sbs.OpenAmConfig;
 import no.nav.sbl.dialogarena.common.jetty.Jetty;
 import no.nav.sbl.dialogarena.common.jetty.Jetty.JettyBuilder;
 import no.nav.sbl.dialogarena.types.Pingable;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.Collection;
 import java.util.function.Consumer;
 
 public interface ApiAppConfigurator {
+
     ApiAppConfigurator sts();
     ApiAppConfigurator sts(StsConfig stsConfig);
 
