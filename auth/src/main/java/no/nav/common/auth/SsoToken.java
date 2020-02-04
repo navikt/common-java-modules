95c169b274364742bd19727f7b7fe040dbbf5879
Alexander Gård <alex.gaard@hotmail.com>
2020-02-04 15:01:42 +0100

    Fjernet modulen auth-openam-sbs

--- auth/src/main/java/no/nav/common/auth/SsoToken.java
+++ auth/src/main/java/no/nav/common/auth/SsoToken.java
@@ -18,25 +18,25 @@ public class SsoToken {
     private final String token;
     private final Map<String, Object> attributes;
 
-    SsoToken(Type type, String token, Map<String, Object> attributes) {
+    SsoToken(Type type, String token, Map<String, ?> attributes) {
         assertNotNull(type);
-        assertNotNull(attributes);
         assertNotNullOrEmpty(token);
+        assertNotNull(attributes);
 
         this.type = type;
         this.token = token;
         this.attributes = unmodifiableMap(attributes);
     }
 
-    public static SsoToken oidcToken(String token, Map<String, Object> attributes) {
+    public static SsoToken oidcToken(String token, Map<String, ?> attributes) {
         return new SsoToken(OIDC, token, attributes);
     }
 
-    public static SsoToken saml(String samlAssertion, Map<String, Object> attributes) {
+    public static SsoToken saml(String samlAssertion, Map<String, ?> attributes) {
         return new SsoToken(SAML, samlAssertion, attributes);
     }
 
-    public static SsoToken eksternOpenAM(String token, Map<String, Object> attributes) {
+    public static SsoToken eksternOpenAM(String token, Map<String, ?> attributes) {
         return new SsoToken(EKSTERN_OPENAM, token, attributes);
     }
 
