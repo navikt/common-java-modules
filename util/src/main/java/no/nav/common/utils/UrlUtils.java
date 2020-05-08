04ad685adfc2b52ccef5e0a60d876a7ff72fc085
Alexander Gård <alex.gaard@hotmail.com>
2020-05-08 14:52:11 +0200

    Kopierte over UrlUtils fra ApiApp

---
+++ util/src/main/java/no/nav/common/utils/UrlUtils.java
@@ -0,0 +1,45 @@
+package no.nav.common.utils;
+
+public class UrlUtils {
+
+    public static String clusterUrlForApplication(String applicationName) {
+        return String.format("http://%s.%s.svc.nais.local",
+                AssertUtils.assertNotNull(applicationName),
+                EnvironmentUtils.requireNamespace()
+        );
+    }
+
+    public static String sluttMedSlash(String path) {
+        if (path == null) {
+            return "/";
+        } else {
+            return path.endsWith("/") ? path : path + "/";
+        }
+    }
+
+    public static String startMedSlash(String path) {
+        if (path == null) {
+            return "/";
+        } else {
+            return path.startsWith("/") ? path : "/" + path;
+        }
+    }
+
+    public static String joinPaths(String... paths) {
+        if (paths == null) {
+            return "/";
+        }
+
+        boolean lastEndsWithSlash = false;
+        StringBuilder sb = new StringBuilder();
+        for (String path : paths) {
+            if (path == null || path.isEmpty() || "/".equals(path)) {
+                continue;
+            }
+            sb.append(lastEndsWithSlash ? path.startsWith("/") ? path.substring(1) : path : path.contains("://") ? path : startMedSlash(path));
+            lastEndsWithSlash = path.endsWith("/");
+        }
+        return sb.length() == 0 ? "/" : sb.toString();
+    }
+
+}
---
+++ util/src/test/java/no/nav/common/utils/UrlUtilsTest.java
@@ -0,0 +1,66 @@
+package no.nav.common.utils;
+
+import no.nav.common.test.junit.SystemPropertiesRule;
+import org.junit.Test;
+
+import static no.nav.common.utils.EnvironmentUtils.NAIS_NAMESPACE_PROPERTY_NAME;
+import static no.nav.common.utils.UrlUtils.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class UrlUtilsTest {
+
+    public SystemPropertiesRule systemPropertiesRule = new SystemPropertiesRule();
+
+    @Test
+    public void clusterUrlForApplication_() {
+        assertThatThrownBy(() -> clusterUrlForApplication("app1")).hasMessageContaining(NAIS_NAMESPACE_PROPERTY_NAME);
+
+        systemPropertiesRule.setProperty(NAIS_NAMESPACE_PROPERTY_NAME, "q0");
+        assertThat(clusterUrlForApplication("app1")).isEqualTo("http://app1.q0.svc.nais.local");
+        assertThat(clusterUrlForApplication("app2")).isEqualTo("http://app2.q0.svc.nais.local");
+
+        systemPropertiesRule.setProperty(NAIS_NAMESPACE_PROPERTY_NAME, "default");
+        assertThat(clusterUrlForApplication("app1")).isEqualTo("http://app1.default.svc.nais.local");
+        assertThat(clusterUrlForApplication("app2")).isEqualTo("http://app2.default.svc.nais.local");
+    }
+
+    @Test
+    public void sluttMedSlash_() {
+        assertThat(sluttMedSlash(null)).isEqualTo("/");
+        assertThat(sluttMedSlash("")).isEqualTo("/");
+        assertThat(sluttMedSlash("/")).isEqualTo("/");
+        assertThat(sluttMedSlash("/abc")).isEqualTo("/abc/");
+    }
+
+    @Test
+    public void startMedSlash_() {
+        assertThat(startMedSlash(null)).isEqualTo("/");
+        assertThat(startMedSlash("")).isEqualTo("/");
+        assertThat(startMedSlash("abc")).isEqualTo("/abc");
+        assertThat(startMedSlash("/abc")).isEqualTo("/abc");
+    }
+
+
+    @Test
+    public void joinPaths_() {
+        assertThat(joinPaths(null)).isEqualTo("/");
+        assertThat(joinPaths(null,null,null)).isEqualTo("/");
+        assertThat(joinPaths("/","","/","/")).isEqualTo("/");
+        assertThat(joinPaths("abc","def")).isEqualTo("/abc/def");
+        assertThat(joinPaths("/abc","def")).isEqualTo("/abc/def");
+        assertThat(joinPaths("abc/","def")).isEqualTo("/abc/def");
+        assertThat(joinPaths("abc","/def")).isEqualTo("/abc/def");
+        assertThat(joinPaths("abc","def/")).isEqualTo("/abc/def/");
+        assertThat(joinPaths("/abc/","/def/")).isEqualTo("/abc/def/");
+        assertThat(joinPaths("/abc/","","/def/")).isEqualTo("/abc/def/");
+        assertThat(joinPaths("/abc/","/","/def/")).isEqualTo("/abc/def/");
+        assertThat(joinPaths("http://","abc","def")).isEqualTo("http://abc/def");
+        assertThat(joinPaths("http://abc","def","ghi")).isEqualTo("http://abc/def/ghi");
+        assertThat(joinPaths("http://abc/","/def","ghi")).isEqualTo("http://abc/def/ghi");
+        assertThat(joinPaths("http://abc/","def","ghi")).isEqualTo("http://abc/def/ghi");
+        assertThat(joinPaths("http://abc/","def/","ghi")).isEqualTo("http://abc/def/ghi");
+        assertThat(joinPaths("ftp://","/abc","def")).isEqualTo("ftp://abc/def");
+    }
+
+}
