f946d36fa0606f6e25789665b50fa0f42e1069ee
Alexander Gård <alex.gaard@hotmail.com>
2021-03-05 09:57:23 +0100

    Forbedring av kafka consumer client
    * Bytt ut busy-wait med lock
    * Shutdown hook
    * Start/stop
    * Rebalancing

--- kafka/src/main/java/no/nav/common/kafka/consumer/KafkaConsumerClient.java
+++ kafka/src/main/java/no/nav/common/kafka/consumer/KafkaConsumerClient.java
@@ -1,28 +1,43 @@
 package no.nav.common.kafka.consumer;
 
-import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.TopicPartition;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class KafkaConsumerClient<K, V> {
+public class KafkaConsumerClient<K, V> implements ConsumerRebalanceListener {
+
+    private enum ClientStatus {
+        NOT_STARTED, RUNNING, STOPPED
+    }
+
+    private final Thread shutdownThread = new Thread(this::stop);
 
     private final KafkaConsumer<K, V> consumer;
-
-    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
 
     private final Map<String, TopicConsumer<K, V>> topicConsumers;
 
     private final Map<String, ExecutorService> topicConsumptionExecutors;
+
+    private final ExecutorService pollExecutor = Executors.newSingleThreadExecutor();
+
+    private final Map<TopicPartition, OffsetAndMetadata> currentOffsets = new ConcurrentHashMap<>();
+
+    private final Set<TopicPartition> revokedOrFailedPartitions = ConcurrentHashMap.newKeySet();
+
+    private final Lock consumptionLock = new ReentrantLock();
+
+    private final AtomicInteger processedRecordCounter = new AtomicInteger();
+
+    private volatile ClientStatus clientStatus = ClientStatus.NOT_STARTED;
 
     public KafkaConsumerClient(KafkaConsumerClientConfig<K, V> config) {
         consumer = new KafkaConsumer<>(config.properties);
@@ -30,38 +45,82 @@ public class KafkaConsumerClient<K, V> {
         topicConsumers = config.topicListeners;
         topicConsumptionExecutors = createTopicExecutors(config.topicListeners.keySet());
 
-        executorService.submit(this::consumeTopics);
+        Runtime.getRuntime().addShutdownHook(shutdownThread);
+    }
+
+    public void start() {
+        if (clientStatus == ClientStatus.RUNNING) {
+            return;
+        }
+
+        clientStatus = ClientStatus.RUNNING;
+
+        // TODO: Log
+
+        pollExecutor.submit(this::consumeTopics);
+    }
+
+    public void stop() {
+        if (clientStatus == ClientStatus.NOT_STARTED || clientStatus == ClientStatus.STOPPED) {
+            return;
+        }
+
+        clientStatus = ClientStatus.STOPPED;
+
+        // TODO: Log
+        try {
+            pollExecutor.awaitTermination(30, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            // TODO: Log
+        }
+    }
+
+    @Override
+    public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+        revokedOrFailedPartitions.addAll(partitions);
+        consumer.commitSync(currentOffsets);
+        // TODO: Log info
+    }
+
+    @Override
+    public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+       // TODO: Log info
     }
 
     private void consumeTopics() {
-        /**
-         * This is a short-hand for {@link #subscribe(Collection, ConsumerRebalanceListener)}, which
-         * uses a no-op listener. If you need the ability to seek to particular offsets, you should prefer
-         * {@link #subscribe(Collection, ConsumerRebalanceListener)}, since group rebalances will cause partition offsets
-         * to be reset. You should also provide your own listener if you are doing your own offset
-         * management since the listener gives you an opportunity to commit offsets before a rebalance finishes.
-         */
-        consumer.subscribe(new ArrayList<>(topicConsumers.keySet()));
+        consumptionLock.unlock();
+        consumer.subscribe(new ArrayList<>(topicConsumers.keySet()), this);
 
         while (true) {
+            revokedOrFailedPartitions.clear();
+            currentOffsets.clear();
+            processedRecordCounter.set(0);
+
             ConsumerRecords<K, V> records = consumer.poll(Duration.ofMillis(1000));
 
             if (records.isEmpty()) {
                 continue;
             }
 
-            Map<String, Object> topicRecordStatus;
-            AtomicInteger counter = new AtomicInteger();
+            int totalRecords = records.count();
 
             for (ConsumerRecord<K, V> record : records) {
                 String topic = record.topic();
                 TopicConsumer<K, V> topicConsumer = topicConsumers.get(topic);
                 ExecutorService executor = topicConsumptionExecutors.get(topic);
+                TopicPartition topicPartition = new TopicPartition(record.topic(), record.partition());
 
-                /**
-                    Hent ut fra map
-                    Hvis siste record på denne topicen har feilet så hopp over
+                /*
+                    If the client is stopped then fast-track through the remaining records so that
+                     we can wait for those that are already being consumed after the while-loop.
+
+                    If some previous record on the same topic + partition has failed to be consumed or has been revoked during rebalancing,
+                    then we cannot consume any more records for this topic + partition for the duration of this poll.
                  */
+                if (clientStatus == ClientStatus.STOPPED || revokedOrFailedPartitions.contains(topicPartition)) {
+                    incrementProcessedRecords(totalRecords);
+                    continue;
+                }
 
                 executor.submit(() -> {
                     ConsumeStatus status;
@@ -69,34 +128,35 @@ public class KafkaConsumerClient<K, V> {
                     try {
                         status = topicConsumer.consume(record);
                     } catch (Exception e) {
+                        // TODO: Log something here
                         status = ConsumeStatus.FAILED;
                     } finally {
-                        counter.incrementAndGet();
+                        incrementProcessedRecords(totalRecords);
                     }
 
-                    // Add to map
-
+                    if (status == ConsumeStatus.OK) {
+                        OffsetAndMetadata offsetAndMetadata = new OffsetAndMetadata(record.offset());
+                        currentOffsets.put(topicPartition, offsetAndMetadata);
+                    } else {
+                        revokedOrFailedPartitions.add(topicPartition);
+                    }
                 });
             }
 
-            while (counter.get() < records.count()) {
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
+            // TODO: All must be wrapped in try catch so that it is guaranteed that the lock will be unlocked
 
-            // Commit offset pr topic basert på status i map
+            consumptionLock.lock();
+
+            // TODO: Maybe log something here?
+            consumer.commitSync(currentOffsets);
         }
     }
 
-    private void consume() {
-
-    }
-
-    public void close() {
-        consumer.close();
+    private void incrementProcessedRecords(int totalRecords) {
+        int processedRecords = processedRecordCounter.incrementAndGet();
+        if (processedRecords >= totalRecords) {
+            consumptionLock.unlock();
+        }
     }
 
     private static Map<String, ExecutorService> createTopicExecutors(Iterable<String> topics) {
