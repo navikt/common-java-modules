e3ff778c7c344f3c2f3c05ff228a78e70ca94f40
Alexander Gård <alex.gaard@hotmail.com>
2021-03-05 14:12:31 +0100

    Fikset på race conditions

--- kafka/src/main/java/no/nav/common/kafka/consumer/KafkaConsumerClient.java
+++ kafka/src/main/java/no/nav/common/kafka/consumer/KafkaConsumerClient.java
@@ -5,21 +5,26 @@ import org.apache.kafka.common.TopicPartition;
 
 import java.time.Duration;
 import java.util.*;
-import java.util.concurrent.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class KafkaConsumerClient<K, V> implements ConsumerRebalanceListener {
 
+    public final static long DEFAULT_POLL_DURATION_MS = 1000;
+
     private enum ClientStatus {
         NOT_STARTED, RUNNING, STOPPED
     }
 
-    private final Thread shutdownThread = new Thread(this::stop);
+    private final long pollDurationMs;
 
     private final KafkaConsumer<K, V> consumer;
 
-    private final Map<String, TopicConsumer<K, V>> topicConsumers;
+    private final Map<String, TopicConsumer<K, V>> topics;
 
     private final ExecutorService pollExecutor = Executors.newSingleThreadExecutor();
 
@@ -31,15 +36,18 @@ public class KafkaConsumerClient<K, V> implements ConsumerRebalanceListener {
 
     private final AtomicInteger processedRecordCounter = new AtomicInteger();
 
-    private Map<String, ThreadPoolExecutor> topicConsumptionExecutors;
+    private Map<String, ExecutorService> topicConsumptionExecutors;
 
     private volatile ClientStatus clientStatus = ClientStatus.NOT_STARTED;
 
     public KafkaConsumerClient(KafkaConsumerClientConfig<K, V> config) {
-        consumer = new KafkaConsumer<>(config.properties);
-        topicConsumers = config.topicListeners;
+        validateConfig(config);
 
-        Runtime.getRuntime().addShutdownHook(shutdownThread);
+        pollDurationMs = config.pollDurationMs;
+        consumer = new KafkaConsumer<>(config.properties);
+        topics = config.topics;
+
+        Runtime.getRuntime().addShutdownHook(new Thread(this::stop));
     }
 
     public void start() {
@@ -47,7 +55,7 @@ public class KafkaConsumerClient<K, V> implements ConsumerRebalanceListener {
             return;
         }
 
-        topicConsumptionExecutors = createTopicExecutors(topicConsumers.keySet());
+        topicConsumptionExecutors = createTopicExecutors(topics.keySet());
 
         clientStatus = ClientStatus.RUNNING;
 
@@ -66,12 +74,10 @@ public class KafkaConsumerClient<K, V> implements ConsumerRebalanceListener {
         // TODO: Log
 
         try {
-            topicConsumptionExecutors.values().forEach(this::drainWaitingTasks);
             pollExecutor.awaitTermination(30, TimeUnit.SECONDS);
             // TODO: Log that everything was finished
         } catch (InterruptedException e) {
-            topicConsumptionExecutors.values().forEach(ThreadPoolExecutor::shutdownNow);
-            consumer.commitSync(currentOffsets);
+            commitCurrentOffsets();
             // TODO: Log
         }
     }
@@ -79,88 +85,94 @@ public class KafkaConsumerClient<K, V> implements ConsumerRebalanceListener {
     @Override
     public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
         revokedOrFailedPartitions.addAll(partitions);
-        consumer.commitSync(currentOffsets);
+        commitCurrentOffsets();
         // TODO: Log info
     }
 
     @Override
     public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
-       // TODO: Log info
+        // TODO: Log info
     }
 
     private void consumeTopics() {
-        consumptionLock.unlock();
-        consumer.subscribe(new ArrayList<>(topicConsumers.keySet()), this);
+        try {
+            consumptionLock.unlock();
+            consumer.subscribe(new ArrayList<>(topics.keySet()), this);
 
-        while (true) {
-            revokedOrFailedPartitions.clear();
-            currentOffsets.clear();
-            processedRecordCounter.set(0);
+            while (clientStatus == ClientStatus.RUNNING) {
+                revokedOrFailedPartitions.clear();
+                currentOffsets.clear();
+                processedRecordCounter.set(0);
 
-            ConsumerRecords<K, V> records = consumer.poll(Duration.ofMillis(1000));
+                // TODO: Wrap this with try-catch so that the main loop does not stop?
+                ConsumerRecords<K, V> records = consumer.poll(Duration.ofMillis(pollDurationMs));
 
-            if (records.isEmpty()) {
-                continue;
-            }
-
-            int totalRecords = records.count();
-
-            for (ConsumerRecord<K, V> record : records) {
-                String topic = record.topic();
-                TopicConsumer<K, V> topicConsumer = topicConsumers.get(topic);
-                ExecutorService executor = topicConsumptionExecutors.get(topic);
-                TopicPartition topicPartition = new TopicPartition(record.topic(), record.partition());
-
-                /*
-                    If the client is stopped then fast-track through the remaining records so that
-                     we can wait for those that are already being consumed after the while-loop.
-
-                    If some previous record on the same topic + partition has failed to be consumed or has been revoked during rebalancing,
-                    then we cannot consume any more records for this topic + partition for the duration of this poll.
-                 */
-                if (clientStatus == ClientStatus.STOPPED || revokedOrFailedPartitions.contains(topicPartition)) {
-                    incrementProcessedRecords(totalRecords);
+                if (records.isEmpty()) {
                     continue;
                 }
 
-                executor.submit(() -> {
-                    ConsumeStatus status;
+                int totalRecords = records.count();
 
-                    try {
-                        status = topicConsumer.consume(record);
-                    } catch (Exception e) {
-                        // TODO: Log something here
-                        status = ConsumeStatus.FAILED;
-                    } finally {
-                        incrementProcessedRecords(totalRecords);
-                    }
+                for (ConsumerRecord<K, V> record : records) {
+                    String topic = record.topic();
+                    TopicConsumer<K, V> topicConsumer = topics.get(topic);
+                    ExecutorService executor = topicConsumptionExecutors.get(topic);
+                    TopicPartition topicPartition = new TopicPartition(record.topic(), record.partition());
 
-                    if (status == ConsumeStatus.OK) {
-                        OffsetAndMetadata offsetAndMetadata = new OffsetAndMetadata(record.offset());
-                        currentOffsets.put(topicPartition, offsetAndMetadata);
-                    } else {
-                        revokedOrFailedPartitions.add(topicPartition);
-                    }
-                });
+                    executor.submit(() -> {
+                        try {
+                            /*
+                                If the client is stopped then fast-track through the remaining records so that
+                                 we can wait for those that are already being consumed after the while-loop.
+
+                                If some previous record on the same topic + partition has failed to be consumed or has been revoked during rebalancing,
+                                 then we cannot consume any more records for this topic + partition for the duration of this poll.
+
+                                 The finally-clause will make sure that the processed records counter is incremented.
+                            */
+                            if (clientStatus == ClientStatus.STOPPED || revokedOrFailedPartitions.contains(topicPartition)) {
+                                return;
+                            }
+
+                            ConsumeStatus status = consumeRecord(topicConsumer, record);
+
+                            if (status == ConsumeStatus.OK) {
+                                OffsetAndMetadata offsetAndMetadata = new OffsetAndMetadata(record.offset());
+                                currentOffsets.put(topicPartition, offsetAndMetadata);
+                            } else {
+                                revokedOrFailedPartitions.add(topicPartition);
+                            }
+                        } finally {
+                            incrementProcessedRecords(totalRecords);
+                        }
+                    });
+                }
+
+                /*
+                    If there are unprocessed records then we wait until all are processed.
+                    Since the records are consumed async this should always happen,
+                     but we still check so we dont get stuck in a deadlock.
+                */
+                if (processedRecordCounter.get() < totalRecords) {
+                    consumptionLock.lock();
+                }
+
+                // TODO: Maybe log something here?
+
+                commitCurrentOffsets();
             }
-
-            // TODO: All must be wrapped in try catch so that it is guaranteed that the lock will be unlocked
-
-            consumptionLock.lock();
-
-            // TODO: Maybe log something here?
-            consumer.commitSync(currentOffsets);
+        } catch (Exception e) {
+            // TODO: Log
+        } finally {
+            consumer.close();
         }
     }
 
-    private void drainWaitingTasks(ThreadPoolExecutor executor) {
-        executor.shutdown();
-
-        final BlockingQueue<Runnable> blockingQueue = executor.getQueue();
-
-        processedRecordCounter.addAndGet(blockingQueue.size());
-
-        blockingQueue.clear();
+    private void commitCurrentOffsets() {
+        if (currentOffsets.size() > 0) {
+            consumer.commitSync(currentOffsets);
+            currentOffsets.clear();
+        }
     }
 
     private void incrementProcessedRecords(int totalRecords) {
@@ -170,9 +182,28 @@ public class KafkaConsumerClient<K, V> implements ConsumerRebalanceListener {
         }
     }
 
-    private static Map<String, ThreadPoolExecutor> createTopicExecutors(Iterable<String> topics) {
-        Map<String, ThreadPoolExecutor> executorsMap = new HashMap<>();
-        topics.forEach(topic -> executorsMap.put(topic, new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue())));
+    private ConsumeStatus consumeRecord(TopicConsumer<K, V> consumer, ConsumerRecord<K, V> record) {
+        try {
+            return consumer.consume(record);
+        } catch (Exception e) {
+            // TODO: Log something here
+            return ConsumeStatus.FAILED;
+        }
+    }
+
+    private static void validateConfig(KafkaConsumerClientConfig<?, ?> config) {
+        if (config.topics.isEmpty()) {
+            throw new IllegalArgumentException("\"topics\" must contain at least 1 topic");
+        }
+
+        if (config.pollDurationMs <= 0) {
+            throw new IllegalArgumentException("\"pollDurationMs\" must be larger than 0");
+        }
+    }
+
+    private static Map<String, ExecutorService> createTopicExecutors(Iterable<String> topics) {
+        Map<String, ExecutorService> executorsMap = new HashMap<>();
+        topics.forEach(topic -> executorsMap.put(topic, Executors.newSingleThreadExecutor()));
         return executorsMap;
     }
 
--- kafka/src/main/java/no/nav/common/kafka/consumer/KafkaConsumerClientConfig.java
+++ kafka/src/main/java/no/nav/common/kafka/consumer/KafkaConsumerClientConfig.java
@@ -3,10 +3,14 @@ package no.nav.common.kafka.consumer;
 import java.util.Map;
 import java.util.Properties;
 
+import static no.nav.common.kafka.consumer.KafkaConsumerClient.DEFAULT_POLL_DURATION_MS;
+
 public class KafkaConsumerClientConfig<K, V> {
+
+    long pollDurationMs = DEFAULT_POLL_DURATION_MS;
 
     Properties properties;
 
-    Map<String, TopicConsumer<K, V>> topicListeners;
+    Map<String, TopicConsumer<K, V>> topics;
 
 }
