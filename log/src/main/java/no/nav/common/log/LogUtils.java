b9bb0f72c2a4f77f9d94bb9530f586befb7779da
Alexander Gård <alex.gaard@hotmail.com>
2021-04-23 15:26:32 +0200

    La til hjelpefunksjoner for å sette MDC context i tråder

--- log/src/main/java/no/nav/common/log/LogUtils.java
+++ log/src/main/java/no/nav/common/log/LogUtils.java
@@ -3,16 +3,49 @@ package no.nav.common.log;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.core.ContextBase;
+import no.nav.common.utils.fn.UnsafeRunnable;
+import no.nav.common.utils.fn.UnsafeSupplier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.slf4j.spi.MDCAdapter;
 
 import java.util.List;
+import java.util.Map;
 
 public class LogUtils {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LogUtils.class);
 
     public static final String ROOT = "ROOT";
+
+    public static void runWithMDCContext(Map<String, String> mdcContext, UnsafeRunnable runnable) {
+        runWithMDCContext(mdcContext, () -> {
+            runnable.run();
+            return null;
+        });
+    }
+
+    public static <T> T runWithMDCContext(Map<String, String> mdcContext, UnsafeSupplier<T> supplier) {
+        MDCAdapter mdcAdapter = MDC.getMDCAdapter();
+        Map<String, String> prevContext = MDC.getCopyOfContextMap();
+
+        try {
+            if (mdcAdapter != null && mdcContext != null) {
+                mdcAdapter.setContextMap(mdcContext);
+            }
+
+            return supplier.get();
+        } finally {
+            if (mdcAdapter != null) {
+                if (prevContext != null) {
+                    mdcAdapter.setContextMap(prevContext);
+                } else {
+                    mdcAdapter.clear();
+                }
+            }
+        }
+    }
 
     public static void setGlobalLogLevel(Level newLevel) {
         LOGGER.info("global log level: {}", newLevel);
--- log/src/test/java/no/nav/common/log/LogUtilsTest.java
+++ log/src/test/java/no/nav/common/log/LogUtilsTest.java
@@ -5,10 +5,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
+
 import static ch.qos.logback.classic.Level.ERROR;
 import static ch.qos.logback.classic.Level.INFO;
-import static no.nav.common.log.LogUtils.buildMarker;
-import static no.nav.common.log.LogUtils.setGlobalLogLevel;
+import static no.nav.common.log.LogUtils.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class LogUtilsTest {
 
@@ -43,6 +48,50 @@ public class LogUtilsTest {
 
         setGlobalLogLevel(ERROR);
         LOGGER.info("info");
+    }
+
+    @Test
+    public void runWithMDCContext_skal_legge_til_MDC_variabler() throws InterruptedException {
+        MDC.clear();
+        MDC.put("TEST", "hello");
+
+        Map<String, String> contextMap = MDC.getCopyOfContextMap();
+
+        AtomicReference<String> mdcValueRef = new AtomicReference<>();
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        new Thread(() -> {
+            runWithMDCContext(contextMap, () -> {
+                mdcValueRef.set(MDC.get("TEST"));
+                countDownLatch.countDown();
+            });
+        }).start();
+
+        countDownLatch.await();
+
+        assertEquals("hello", mdcValueRef.get());
+    }
+
+    @Test
+    public void runWithMDCContext_skal_rydde_opp_context() throws InterruptedException {
+        MDC.clear();
+
+        Map<String, String> contextMap = MDC.getCopyOfContextMap();
+
+        AtomicReference<String> mdcValueRef = new AtomicReference<>();
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        new Thread(() -> {
+            runWithMDCContext(contextMap, () -> {
+                MDC.put("TEST1", "hello1");
+            });
+            mdcValueRef.set(MDC.get("TEST1"));
+            countDownLatch.countDown();
+        }).start();
+
+        countDownLatch.await();
+
+        assertNull(mdcValueRef.get());
     }
 
 }
