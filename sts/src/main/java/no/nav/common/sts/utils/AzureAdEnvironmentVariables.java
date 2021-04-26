89d94e52aaecf63ad34c0095610fbd79eb5d78df
Alexander Gård <alex.gaard@hotmail.com>
2021-04-26 07:52:20 +0200

    Legg til alle Azure AD miljø variabler som blir injected av NAIS

--- sts/src/main/java/no/nav/common/sts/utils/AzureAdEnvironmentVariables.java
+++ sts/src/main/java/no/nav/common/sts/utils/AzureAdEnvironmentVariables.java
@@ -1,11 +1,43 @@
 package no.nav.common.sts.utils;
 
+/**
+ * Environment variables that are injected into NAIS applications configured with Azure AD.
+ * See: https://doc.nais.io/security/auth/azure-ad/#runtime-variables-credentials
+ */
 public class AzureAdEnvironmentVariables {
 
-    public static String AZURE_OPENID_CONFIG_TOKEN_ENDPOINT = "AZURE_OPENID_CONFIG_TOKEN_ENDPOINT";
+    public final static String AZURE_OPENID_CONFIG_TOKEN_ENDPOINT = "AZURE_OPENID_CONFIG_TOKEN_ENDPOINT";
 
-    public static String AZURE_APP_CLIENT_ID = "AZURE_APP_CLIENT_ID";
+    public final static String AZURE_APP_CLIENT_ID = "AZURE_APP_CLIENT_ID";
 
-    public static String AZURE_APP_CLIENT_SECRET = "AZURE_APP_CLIENT_SECRET";
+    public final static String AZURE_APP_TENANT_ID = "AZURE_APP_TENANT_ID";
+
+    public final static String AZURE_APP_CLIENT_SECRET = "AZURE_APP_CLIENT_SECRET";
+
+    public final static String AZURE_APP_PRE_AUTHORIZED_APPS = "AZURE_APP_PRE_AUTHORIZED_APPS";
+
+    public final static String AZURE_APP_WELL_KNOWN_URL = "AZURE_APP_WELL_KNOWN_URL";
+
+    public final static String AZURE_APP_JWKS = "AZURE_APP_JWKS";
+
+    public final static String AZURE_APP_JWK = "AZURE_APP_JWK";
+
+    public final static String AZURE_OPENID_CONFIG_JWKS_URI = "AZURE_OPENID_CONFIG_JWKS_URI";
+
+    public final static String AZURE_OPENID_CONFIG_ISSUER = "AZURE_OPENID_CONFIG_ISSUER";
+
+    public final static String AZURE_APP_CERTIFICATE_KEY_ID = "AZURE_APP_CERTIFICATE_KEY_ID";
+
+    public final static String AZURE_APP_PASSWORD_KEY_ID = "AZURE_APP_PASSWORD_KEY_ID";
+
+
+
+    public final static String AZURE_APP_NEXT_JWK = "AZURE_APP_NEXT_JWK";
+
+    public final static String AZURE_APP_NEXT_CLIENT_SECRET = "AZURE_APP_NEXT_CLIENT_SECRET";
+
+    public final static String AZURE_APP_NEXT_CERTIFICATE_KEY_ID = "AZURE_APP_NEXT_CERTIFICATE_KEY_ID";
+
+    public final static String AZURE_APP_NEXT_PASSWORD_KEY_ID = "AZURE_APP_NEXT_PASSWORD_KEY_ID";
 
 }
