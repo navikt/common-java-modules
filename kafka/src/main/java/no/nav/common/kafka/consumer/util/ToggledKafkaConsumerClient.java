befe47a764446ea2d4a11896d39d42f6bfc5f39e
Alexander Gård <alex.gaard@hotmail.com>
2021-06-25 13:36:27 +0200

    La til kafka consumer client med støtte for å toggle av/på

---
+++ kafka/src/main/java/no/nav/common/kafka/consumer/util/ToggledKafkaConsumerClient.java
@@ -0,0 +1,77 @@
+package no.nav.common.kafka.consumer.util;
+
+import lombok.extern.slf4j.Slf4j;
+import no.nav.common.kafka.consumer.KafkaConsumerClient;
+
+import java.time.Duration;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+
+/**
+ * {@link KafkaConsumerClient} with support for toggling the consumer on/off based off the result from a {@link Supplier<Boolean>}
+ */
+@Slf4j
+public class ToggledKafkaConsumerClient implements KafkaConsumerClient {
+
+    private final static Duration DEFAULT_POLL_TIMEOUT = Duration.ofMinutes(1);
+
+    private final KafkaConsumerClient kafkaConsumerClient;
+
+    private final Duration pollTimeoutDuration;
+
+    private final Supplier<Boolean> isToggledOnSupplier;
+
+    private ScheduledExecutorService executorService;
+
+    public ToggledKafkaConsumerClient(KafkaConsumerClient kafkaConsumerClient, Supplier<Boolean> isToggledOnSupplier) {
+        this.kafkaConsumerClient = kafkaConsumerClient;
+        this.isToggledOnSupplier = isToggledOnSupplier;
+        this.pollTimeoutDuration = DEFAULT_POLL_TIMEOUT;
+    }
+
+    public ToggledKafkaConsumerClient(KafkaConsumerClient kafkaConsumerClient, Supplier<Boolean> isToggledOnSupplier, Duration pollTimeoutDuration) {
+        this.kafkaConsumerClient = kafkaConsumerClient;
+        this.isToggledOnSupplier = isToggledOnSupplier;
+        this.pollTimeoutDuration = pollTimeoutDuration;
+    }
+
+    @Override
+    public void start() {
+        if (!isRunning()) {
+            kafkaConsumerClient.start();
+            executorService = Executors.newSingleThreadScheduledExecutor();
+            executorService.scheduleAtFixedRate(this::syncRunningStateWithToggle, pollTimeoutDuration.toMillis(), pollTimeoutDuration.toMillis(), TimeUnit.MILLISECONDS);
+        }
+    }
+
+    @Override
+    public void stop() {
+        if (executorService != null) {
+            executorService.shutdown();
+        }
+
+        if (isRunning()) {
+            kafkaConsumerClient.stop();
+        }
+    }
+
+    @Override
+    public boolean isRunning() {
+        return kafkaConsumerClient.isRunning();
+    }
+
+    private void syncRunningStateWithToggle() {
+        boolean isToggledOn = isToggledOnSupplier.get();
+        boolean isRunning = isRunning();
+
+        if (isToggledOn && !isRunning) {
+            log.info("Starting consumer... Toggle is on and kafka consumer client is not running");
+            kafkaConsumerClient.start();
+        } else if (!isToggledOn && isRunning) {
+            log.info("Stopping consumer... Toggle is off and kafka consumer client is running");
+            kafkaConsumerClient.stop();
+        }
+    }
+}
