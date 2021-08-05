476390bb97197d8d8c5302b87a2f426ed4ada98f
matiasvinjevoll <matias.vinjevoll@nav.no>
2021-08-05 16:04:28 +0200

    Test for transaksjonshåndtering med record processor

--- kafka/src/test/java/no/nav/common/kafka/producer/feilhandtering/KafkaProducerRecordProcessorIntegrationTest.java
+++ kafka/src/test/java/no/nav/common/kafka/producer/feilhandtering/KafkaProducerRecordProcessorIntegrationTest.java
@@ -12,11 +12,10 @@ import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.KafkaAdminClient;
 import org.apache.kafka.clients.admin.NewTopic;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.ClassRule;
-import org.junit.Test;
+import org.junit.*;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -42,8 +41,19 @@ public class KafkaProducerRecordProcessorIntegrationTest {
 
     private KafkaProducerRepository producerRepository;
 
+    private KafkaProducerRecordProcessor recordProcessor;
+
+    private KafkaConsumerClientImpl<String, String> consumerClient;
+
+    private AtomicInteger counterTopicA = new AtomicInteger();
+
+    private AtomicInteger counterTopicB = new AtomicInteger();
+
     @Before
     public void setup() {
+        counterTopicA.set(0);
+        counterTopicB.set(0);
+
         String brokerUrl = kafka.getBootstrapServers();
 
         dataSource = LocalOracleH2Database.createDatabase();
@@ -57,6 +67,31 @@ public class KafkaProducerRecordProcessorIntegrationTest {
                 new NewTopic(TEST_TOPIC_B, 1, (short) 1)
         ));
         admin.close(); // Apply changes
+
+        KafkaProducerClient<byte[], byte[]> producer = new KafkaProducerClientImpl<>(kafkaTestByteProducerProperties(kafka.getBootstrapServers()));
+        LeaderElectionClient leaderElectionClient = () -> true;
+
+        recordProcessor = new KafkaProducerRecordProcessor(
+                producerRepository,
+                producer,
+                leaderElectionClient
+        );
+
+        KafkaConsumerClientConfig<String, String> config = new KafkaConsumerClientConfig<>(
+                kafkaTestConsumerProperties(kafka.getBootstrapServers()),
+                Map.of(
+                        TEST_TOPIC_A, (r) -> {
+                            counterTopicA.incrementAndGet();
+                            return ConsumeStatus.OK;
+                        },
+                        TEST_TOPIC_B, (r) -> {
+                            counterTopicB.incrementAndGet();
+                            return ConsumeStatus.OK;
+                        }
+                )
+        );
+
+        consumerClient = new KafkaConsumerClientImpl<>(config);
     }
 
     @After
@@ -73,37 +108,9 @@ public class KafkaProducerRecordProcessorIntegrationTest {
         producerRepository.storeRecord(storedRecord(TEST_TOPIC_B, "value1", "key1"));
         producerRepository.storeRecord(storedRecord(TEST_TOPIC_B, "value2", "key2"));
 
-        KafkaProducerClient<byte[], byte[]> producer = new KafkaProducerClientImpl<>(kafkaTestByteProducerProperties(kafka.getBootstrapServers()));
-        LeaderElectionClient leaderElectionClient = () -> true;
-
-        KafkaProducerRecordProcessor recordProcessor = new KafkaProducerRecordProcessor(
-                producerRepository,
-                producer,
-                leaderElectionClient
-        );
-
         recordProcessor.start();
         Thread.sleep(1000);
         recordProcessor.close();
-
-        AtomicInteger counterTopicA = new AtomicInteger();
-        AtomicInteger counterTopicB = new AtomicInteger();
-
-        KafkaConsumerClientConfig<String, String> config = new KafkaConsumerClientConfig<>(
-                kafkaTestConsumerProperties(kafka.getBootstrapServers()),
-                Map.of(
-                        TEST_TOPIC_A, (r) -> {
-                            counterTopicA.incrementAndGet();
-                            return ConsumeStatus.OK;
-                        },
-                        TEST_TOPIC_B, (r) -> {
-                            counterTopicB.incrementAndGet();
-                            return ConsumeStatus.OK;
-                        }
-                )
-        );
-
-        KafkaConsumerClientImpl<String, String> consumerClient = new KafkaConsumerClientImpl<>(config);
 
         consumerClient.start();
         Thread.sleep(1000);
@@ -113,6 +120,30 @@ public class KafkaProducerRecordProcessorIntegrationTest {
         assertEquals(2, counterTopicB.get());
         assertTrue(producerRepository.getRecords(10).isEmpty());
     }
+
+    @Test
+    public void should_not_send_records_to_kafka_stored_in_a_transaction_that_gets_rolled_back() throws InterruptedException {
+        TransactionTemplate transactionTemplate = new TransactionTemplate(new DataSourceTransactionManager(dataSource));
+
+        consumerClient.start();
+        recordProcessor.start();
+
+        transactionTemplate.execute(status -> {
+            producerRepository.storeRecord(storedRecord(TEST_TOPIC_A, "value1", "key1"));
+            try {
+                Thread.sleep(4000);
+            } catch (InterruptedException ignored) {
+            }
+            status.setRollbackOnly();
+            return null;
+        });
+        Thread.sleep(4000);
+
+        recordProcessor.close();
+        consumerClient.stop();
+        assertEquals(0, counterTopicA.get());
+    }
+
 
     private StoredProducerRecord storedRecord(String topic, String key, String value) {
         return new StoredProducerRecord(
