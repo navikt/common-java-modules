baf28b7c634826e0cffbefcbad6fde0b5f913468
Alexander Gård <alex.gaard@hotmail.com>
2022-03-28 09:29:04 +0200

    Deprecate azure ad funksjoner fra sts-modulen

--- sts/src/main/java/no/nav/common/sts/MsGraphTokenProvider.java
+++ sts/src/main/java/no/nav/common/sts/MsGraphTokenProvider.java
@@ -2,7 +2,9 @@ package no.nav.common.sts;
 
 /**
  * Provides tokens for accessing Microsoft Graph API without a user in context
+ * Deprecated: Succeeded by token-client module
  */
+@Deprecated
 public class MsGraphTokenProvider implements TokenProvider {
 
     private final static String MS_GRAPH_SCOPE = "https://graph.microsoft.com/.default";
