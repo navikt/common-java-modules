acf34bd77cd825028ce2e27157d06e472a6478a4
Alexander Gård <alex.gaard@hotmail.com>
2022-06-03 12:47:39 +0200

    La til dproc, request og requestMethod som CEF extensions

--- audit-log/src/main/java/no/nav/common/audit_log/cef/CefMessageExtensionFields.java
+++ audit-log/src/main/java/no/nav/common/audit_log/cef/CefMessageExtensionFields.java
@@ -8,7 +8,7 @@ public class CefMessageExtensionFields {
     /**
      * CEF-navn: sourceUserId
      * Beskrivelse: Brukeren som utfører en operasjon, kan f.eks være veileder som gjør oppslag på en bruker
-     * Eksempel: NAV-ident
+     * Eksempel: Z12345
      */
     public final static String FIELD_SOURCE_USER_ID = "suid";
 
@@ -22,16 +22,38 @@ public class CefMessageExtensionFields {
 
     /**
      * CEF-navn: endTime
-     * Beskrivelse: Når slutten på eventet oppstod, dette er som oftest bare System.currentTimeMillis()
-     * Eksempel: epoch timestamp in milliseconds
+     * Beskrivelse: Epoch tidstempel for når slutten på eventet oppstod
+     * Eksempel: 1654252356153
      */
     public final static String FIELD_END_TIME = "end";
 
     /**
      * CEF-navn: sourceProcessName
      * Beskrivelse: Id som identifiserer operasjonen, dette kan f.eks være Nav-Call-Id HTTP-headeren som brukes for tracing
-     * Eksempel: unik string som identifiserer operasjonen
+     * Eksempel: 7b3a4c34-1b0c-40fb-bf15-743f3612c350
      */
     public final static String FIELD_SPROC = "sproc";
+
+    /**
+     * NB: Beskrivelse og eksempel er hentet fra CEF dokumentasjon
+     * CEF-navn: destinationProcessName
+     * Beskrivelse: The name of the event's destination process
+     * Eksempel: "telnetd" or "sshd"
+     */
+    public final static String FIELD_DPROC = "dproc";
+
+    /**
+     * CEF-navn: requestUrl
+     * Beskrivelse: URLen til HTTP-requestet som trigget eventet
+     * Eksempel: /api/sak
+     */
+    public final static String FIELD_REQUEST = "request";
+
+    /**
+     * CEF-navn: requestMethod
+     * Beskrivelse: Metoden til HTTP-requestet som trigget eventet
+     * Eksempel: POST
+     */
+    public final static String FIELD_REQUEST_METHOD = "requestMethod";
 
 }
