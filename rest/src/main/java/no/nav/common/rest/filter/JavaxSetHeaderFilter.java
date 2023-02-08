54e28e26957a70cab4f6f781fee0500f1df5aced
JulieHillRoa <julie.hill.roa@nav.no>
2023-02-08 10:36:35 +0100

    Endre fra jakarta- til javaximport i javaxversjon

--- rest/src/main/java/no/nav/common/rest/filter/JavaxSetHeaderFilter.java
+++ rest/src/main/java/no/nav/common/rest/filter/JavaxSetHeaderFilter.java
@@ -1,8 +1,8 @@
 package no.nav.common.rest.filter;
 
-import jakarta.servlet.*;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.util.Map;
