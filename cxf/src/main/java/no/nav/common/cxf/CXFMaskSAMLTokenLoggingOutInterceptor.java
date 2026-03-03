477e97fb8b5f6890cfd881b8cf4bd6294fc02743
Klara Margrethe Helgemo <klara.margrethe.helgemo@nav.no>
2026-03-03 11:10:18 +0100

    Endringer / moderniseringer

--- cxf/src/main/java/no/nav/common/cxf/CXFMaskSAMLTokenLoggingOutInterceptor.java
+++ cxf/src/main/java/no/nav/common/cxf/CXFMaskSAMLTokenLoggingOutInterceptor.java
@@ -2,26 +2,20 @@ package no.nav.common.cxf;
 
 import lombok.Setter;
 import org.apache.cxf.ext.logging.event.LogEvent;
-import org.apache.cxf.ext.logging.event.LogEventSender;
-import org.apache.cxf.ext.logging.slf4j.Slf4jEventSender;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.parser.Parser;
 
-
-public class CXFMaskSAMLTokenLoggingOutInterceptor implements LogEventSender {
-    @Setter
+@Setter
+public class CXFMaskSAMLTokenLoggingOutInterceptor extends AbstractMaskingLogEventSender {
     private boolean maskerSAMLToken = true;
 
-    private final LogEventSender delegate = new Slf4jEventSender();
-
     @Override
-    public void send(LogEvent event) {
+    protected void mask(LogEvent event) {
         if (maskerSAMLToken && event.getContentType() != null && event.getContentType().contains("xml")) {
             event.setPayload(removeSAMLTokenFromXML(event.getPayload()));
         }
-        delegate.send(event);
     }
 
     private String removeSAMLTokenFromXML(String xmlString) {
--- cxf/src/main/java/no/nav/common/cxf/CXFMaskTokenLoggingInInterceptor.java
+++ cxf/src/main/java/no/nav/common/cxf/CXFMaskTokenLoggingInInterceptor.java
@@ -1,23 +1,18 @@
 package no.nav.common.cxf;
 
 import org.apache.cxf.ext.logging.event.LogEvent;
-import org.apache.cxf.ext.logging.event.LogEventSender;
-import org.apache.cxf.ext.logging.slf4j.Slf4jEventSender;
 
-public class CXFMaskTokenLoggingInInterceptor implements LogEventSender {
+public class CXFMaskTokenLoggingInInterceptor extends AbstractMaskingLogEventSender {
     private boolean maskerToken = true;
-
-    private final LogEventSender delegate = new Slf4jEventSender();
 
     public void setMaskerTokenLogging(boolean maskerTokenLogging) {
         this.maskerToken = maskerTokenLogging;
     }
 
     @Override
-    public void send(LogEvent event) {
+    protected void mask(LogEvent event) {
         if (maskerToken && event.getHeaders() != null) {
             event.getHeaders().remove("Cookie");
         }
-        delegate.send(event);
     }
 }
