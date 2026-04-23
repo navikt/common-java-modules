b638a8e5a9420c5c7e3b2e92d6d9433afca32906
Tia Firing <tia.firing@nav.no>
2026-04-23 06:47:59 +0200

    legger til kotlinmodule så mapperen fungerer bedre med kotlin

--- json/src/main/java/no/nav/common/json/JsonMapper.java
+++ json/src/main/java/no/nav/common/json/JsonMapper.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import tools.jackson.databind.DeserializationFeature;
 import tools.jackson.databind.MapperFeature;
 import tools.jackson.databind.ObjectMapper;
+import tools.jackson.module.kotlin.KotlinModule;
 
 public class JsonMapper {
 
@@ -14,6 +15,7 @@ public class JsonMapper {
     public static ObjectMapper applyDefaultConfiguration(ObjectMapper objectMapper) {
         return objectMapper.rebuild()
                 .addModule(DateModule.module())
+                .addModule(new KotlinModule.Builder().build())
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
                 .configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true)
                 .configure(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY, false)
